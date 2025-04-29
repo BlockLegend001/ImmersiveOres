db5a90cb4ef8a745c19997171dd832d2075d4a97
BlockLegend001 <rosarioarcamone@gmail.com>
2025-04-29 11:52:53 +0200

    Added Highlights Outline to the Blocks

--- src/main/java/com/blocklegend001/immersiveores/ImmersiveOresClient.java
+++ src/main/java/com/blocklegend001/immersiveores/ImmersiveOresClient.java
@@ -1,12 +1,16 @@
 package com.blocklegend001.immersiveores;
 
 import com.blocklegend001.immersiveores.util.ModItemProperties;
+import com.blocklegend001.immersiveores.util.tools.excavator.ExcavatorOverlayRenderer;
+import com.blocklegend001.immersiveores.util.tools.hammer.HammerOverlayRenderer;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ImmersiveOresClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        ExcavatorOverlayRenderer.init();
+        HammerOverlayRenderer.init();
         ModItemProperties.registerModModels();
     }
 }
