c5c7d4e03c9d5b498055234cb60736b283d658e6
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-01 17:28:11 +0200

    Fixed effects doesnt work properly on armor

--- src/main/java/com/blocklegend001/immersiveores/item/custom/vibranium/VibraniumArmor.java
+++ src/main/java/com/blocklegend001/immersiveores/item/custom/vibranium/VibraniumArmor.java
@@ -35,10 +35,10 @@ public class VibraniumArmor extends ArmorItem {
             if(entity instanceof PlayerEntity player) {
                 if (player.getInventory().getArmorStack(0).getItem() == ModItems.VIBRANIUM_BOOTS) {
                     if (VibraniumConfig.speedIVibraniumArmor) {
-                        player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 0, 0, false, false));
+                        player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 400, 0, false, false));
                     }
                     if (VibraniumConfig.jumpIVibraniumArmor) {
-                        player.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 0, 0, false, false));
+                        player.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 400, 0, false, false));
                     }
                 }
                 if (player.getInventory().getArmorStack(3).getItem() == ModItems.VIBRANIUM_HELMET) {
