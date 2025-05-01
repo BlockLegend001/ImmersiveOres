817d9bb9c00715062e726a91438f6fc14463d914
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-01 17:53:26 +0200

    Added Highlights Outline to the Blocks

--- src/main/java/com/blocklegend001/immersiveores/ImmersiveOres.java
+++ src/main/java/com/blocklegend001/immersiveores/ImmersiveOres.java
@@ -7,6 +7,7 @@ import com.blocklegend001.immersiveores.config.VulpusConfig;
 import com.blocklegend001.immersiveores.event.ModEventHandler;
 import com.blocklegend001.immersiveores.item.ModCreativeModTabs;
 import com.blocklegend001.immersiveores.item.ModItems;
+import com.blocklegend001.immersiveores.util.OverlayRender;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -24,6 +25,7 @@ public class ImmersiveOres {
         EnderiumConfig.loadConfig();
         ModBlocks.register(modEventBus);
         ModItems.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(OverlayRender.class);
         MinecraftForge.EVENT_BUS.register(ModEventHandler.class);
         MinecraftForge.EVENT_BUS.register(this);
     }
