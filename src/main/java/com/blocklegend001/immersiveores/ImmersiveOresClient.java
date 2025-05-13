3e15213bdd935ddf352b9df86fd4e2c0a0598361
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-13 12:42:48 +0200

    If the player is sneaking (shift held), the Excavator/Hammer will only break the single targeted block.

--- src/main/java/com/blocklegend001/immersiveores/ImmersiveOresClient.java
+++ src/main/java/com/blocklegend001/immersiveores/ImmersiveOresClient.java
@@ -1,9 +1,11 @@
 package com.blocklegend001.immersiveores;
 
 import com.blocklegend001.immersiveores.util.ModItemProperties;
-import com.blocklegend001.immersiveores.util.tools.excavator.ExcavatorOverlayRenderer;
-import com.blocklegend001.immersiveores.util.tools.hammer.HammerOverlayRenderer;
+import com.blocklegend001.immersiveores.util.tools.excavator.*;
+import com.blocklegend001.immersiveores.util.tools.hammer.*;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.util.ActionResult;
 
 public class ImmersiveOresClient implements ClientModInitializer {
 
@@ -12,5 +14,18 @@ public class ImmersiveOresClient implements ClientModInitializer {
         ModItemProperties.registerModModels();
         ExcavatorOverlayRenderer.init();
         HammerOverlayRenderer.init();
+
+        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+            if (world.isClient) {
+                ModEventsVibraniumExcavator.isSneaking = player.isSneaking();
+                ModEventsVulpusExcavator.isSneaking = player.isSneaking();
+                ModEventsEnderiumExcavator.isSneaking = player.isSneaking();
+
+                ModEventsVibraniumHammer.isSneaking = player.isSneaking();
+                ModEventsVulpusHammer.isSneaking = player.isSneaking();
+                ModEventsEnderiumHammer.isSneaking = player.isSneaking();
+            }
+            return ActionResult.PASS;
+        });
     }
 }
--- src/main/java/com/blocklegend001/immersiveores/util/tools/hammer/ModEventsVulpusHammer.java
+++ src/main/java/com/blocklegend001/immersiveores/util/tools/hammer/ModEventsVulpusHammer.java
@@ -16,26 +16,34 @@ import java.util.Set;
 
 public class ModEventsVulpusHammer implements PlayerBlockBreakEvents.Before{
     private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
+    public static boolean isSneaking = false;
 
     @Override
     public boolean beforeBlockBreak(World world, PlayerEntity player, BlockPos pos,
                                     BlockState state, @Nullable BlockEntity blockEntity) {
+        if (!(player instanceof ServerPlayerEntity serverPlayer)) return true;
+
         ItemStack mainHandItem = player.getMainHandStack();
+        if (!(mainHandItem.getItem() instanceof VulpusHammer hammer)) return true;
 
-        if(mainHandItem.getItem() instanceof VulpusHammer hammer && player instanceof ServerPlayerEntity serverPlayer) {
-            if(HARVESTED_BLOCKS.contains(pos)) {
-                return true;
+        if (HARVESTED_BLOCKS.contains(pos)) return true;
+
+        HARVESTED_BLOCKS.add(pos);
+
+        try {
+            int radius = isSneaking ? 0 : 2;
+            for (BlockPos targetPos : VulpusHammer.getBlocksToBeDestroyed(radius, pos, serverPlayer)) {
+                if (targetPos.equals(pos)) continue;
+
+                if (HARVESTED_BLOCKS.contains(targetPos)) continue;
+                if (!hammer.isCorrectForDrops(mainHandItem, world.getBlockState(targetPos))) continue;
+
+                HARVESTED_BLOCKS.add(targetPos);
+                serverPlayer.interactionManager.tryBreakBlock(targetPos);
+                HARVESTED_BLOCKS.remove(targetPos);
             }
-
-            for(BlockPos position : VulpusHammer.getBlocksToBeDestroyed(2, pos, serverPlayer)) {
-                if(pos == position || !hammer.isCorrectForDrops(mainHandItem, world.getBlockState(position))) {
-                    continue;
-                }
-
-                HARVESTED_BLOCKS.add(position);
-                serverPlayer.interactionManager.tryBreakBlock(position);
-                HARVESTED_BLOCKS.remove(position);
-            }
+        } finally {
+            HARVESTED_BLOCKS.remove(pos);
         }
 
         return true;
