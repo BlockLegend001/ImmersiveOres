dbd8fe4c3a53567f971c8c51dcbe26921c00b1c6
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-29 22:18:53 +0200

    Now when you craft an armor the enchants will be transferred to the old armor to the new one

--- src/main/java/com/blocklegend001/immersiveores/item/custom/vibranium/VibraniumArmor.java
+++ src/main/java/com/blocklegend001/immersiveores/item/custom/vibranium/VibraniumArmor.java
@@ -5,16 +5,14 @@ import com.blocklegend001.immersiveores.item.ModItems;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 
 import java.util.List;
@@ -43,6 +41,32 @@ public class VibraniumArmor extends ArmorItem {
                 if (VibraniumConfig.nightVisionVibraniumArmor) {
                     player.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0, false, false));
                 }
+            }
+        }
+    }
+
+    @Override
+    public void onCraftedBy(ItemStack stack, Level level, Player player) {
+        super.onCraftedBy(stack, level, player);
+
+        for (int i = 1; i <= 9; i++) {
+            ItemStack ingredient = player.containerMenu.getSlot(i).getItem();
+
+            if (ingredient.getItem() instanceof ArmorItem armorItem &&
+                    armorItem.getMaterial() == ArmorMaterials.NETHERITE &&
+                    armorItem.getType() == ((ArmorItem) stack.getItem()).getType()) {
+
+                var ench = ingredient.get(DataComponents.ENCHANTMENTS);
+                if (ench != null) {
+                    stack.set(DataComponents.ENCHANTMENTS, ench);
+                }
+
+                var customName = ingredient.get(DataComponents.CUSTOM_NAME);
+                if (customName != null) {
+                    stack.set(DataComponents.CUSTOM_NAME, customName);
+                }
+
+                break;
             }
         }
     }
