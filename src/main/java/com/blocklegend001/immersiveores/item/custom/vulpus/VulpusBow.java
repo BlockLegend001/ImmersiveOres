e19795272061bf7520c71aa066095868f3c0c313
BlockLegend001 <rosarioarcamone@gmail.com>
2025-06-10 18:38:15 +0200

    Now Infinity works on bows

--- src/main/java/com/blocklegend001/immersiveores/item/custom/vulpus/VulpusBow.java
+++ src/main/java/com/blocklegend001/immersiveores/item/custom/vulpus/VulpusBow.java
@@ -54,7 +54,7 @@ public class VulpusBow extends BowItem {
             float arrowVelocity = getPullProgress(charge);
 
             if (arrowVelocity >= 0.1) {
-                int arrowCount = 4;
+                int arrowCount = 3;
                 for (int i = 0; i < arrowCount; i++) {
                     ArrowItem arrowItem = (ArrowItem) (arrowStack.getItem() instanceof ArrowItem ? arrowStack.getItem() : Items.ARROW);
                     PersistentProjectileEntity arrowEntity = arrowItem.createArrow(world, arrowStack, user, stack);
