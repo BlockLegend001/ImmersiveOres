195fecd8ffd3436d3a75b9813ebe410428cad516
BlockLegend001 <rosarioarcamone@gmail.com>
2025-06-21 18:01:00 +0200

    A lot of changes, check CHANGELOG.md

--- src/main/java/com/blocklegend001/immersiveores/item/custom/vibranium/VibraniumBow.java
+++ src/main/java/com/blocklegend001/immersiveores/item/custom/vibranium/VibraniumBow.java
@@ -12,6 +12,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
@@ -112,6 +113,8 @@ public class VibraniumBow extends BowItem {
                         player.getInventory().removeItem(arrowStack);
                     }
                 }
+                player.getMainHandItem().hurtAndBreak(1, player,
+                        LivingEntity.getSlotForHand(InteractionHand.MAIN_HAND));
             }
         }
         return false;
