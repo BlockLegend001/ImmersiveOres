d625f579a45caf37fca9a28bcb9c06d64ce311e8
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-02 10:26:29 +0200

    A lot of changes, check CHANGELOG.md

--- src/main/java/com/blocklegend001/immersiveores/item/custom/vulpus/VulpusHammer.java
+++ src/main/java/com/blocklegend001/immersiveores/item/custom/vulpus/VulpusHammer.java
@@ -48,7 +48,7 @@ public class VulpusHammer extends Hammer {
             components.add(Component.translatable("tooltip.immersiveores.immunetofire.tooltip").withStyle(ChatFormatting.RED));
 
             Component text = Component.literal("Dig area: ")
-                    .withStyle(ChatFormatting.GRAY)
+                    .withStyle(ChatFormatting.RED)
                     .append(Component.literal(widht + "x1").withStyle(ChatFormatting.YELLOW));
 
             components.add(text);
