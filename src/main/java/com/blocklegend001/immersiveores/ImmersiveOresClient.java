5d5764494645ea547ff54bb2bf73a4ea7d65eb31
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-21 08:40:47 +0200

    Fixed a bug that not let the player break one block when shifting in multiplayer

--- src/main/java/com/blocklegend001/immersiveores/ImmersiveOresClient.java
+++ src/main/java/com/blocklegend001/immersiveores/ImmersiveOresClient.java
@@ -4,8 +4,6 @@ import com.blocklegend001.immersiveores.util.tools.bow.ModItemProperties;
 import com.blocklegend001.immersiveores.util.tools.excavator.*;
 import com.blocklegend001.immersiveores.util.tools.hammer.*;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
-import net.minecraft.util.ActionResult;
 
 public class ImmersiveOresClient implements ClientModInitializer {
 
@@ -14,18 +12,5 @@ public class ImmersiveOresClient implements ClientModInitializer {
         ModItemProperties.registerModModels();
         ExcavatorOverlayRenderer.init();
         HammerOverlayRenderer.init();
-
-        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
-            if (world.isClient) {
-                ModEventsVibraniumExcavator.isSneaking = player.isSneaking();
-                ModEventsVulpusExcavator.isSneaking = player.isSneaking();
-                ModEventsEnderiumExcavator.isSneaking = player.isSneaking();
-
-                ModEventsVibraniumHammer.isSneaking = player.isSneaking();
-                ModEventsVulpusHammer.isSneaking = player.isSneaking();
-                ModEventsEnderiumHammer.isSneaking = player.isSneaking();
-            }
-            return ActionResult.PASS;
-        });
     }
 }
