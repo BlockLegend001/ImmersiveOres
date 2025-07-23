acc397258300e542da9e0da154bf468e68c1f293
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-23 10:16:42 +0200

    Added 1.21.8-Forge

---
+++ src/main/java/com/blocklegend001/immersiveores/config/VibraniumConfig.java
@@ -0,0 +1,247 @@
+package com.blocklegend001.immersiveores.config;
+
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.io.WritingMode;
+import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class VibraniumConfig {
+
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    // 1. Armor Protection and Resistance
+    public static final ForgeConfigSpec.IntValue toughnessValueVibraniumArmor;
+    public static final ForgeConfigSpec.IntValue enchantmentValueVibraniumArmor;
+    public static final ForgeConfigSpec.IntValue knockbackResistanceValueVibraniumArmor;
+    public static final ForgeConfigSpec.IntValue protectionValueVibraniumBoots;
+    public static final ForgeConfigSpec.IntValue protectionValueVibraniumLeggings;
+    public static final ForgeConfigSpec.IntValue protectionValueVibraniumChestplate;
+    public static final ForgeConfigSpec.IntValue protectionValueVibraniumHelmet;
+
+    // 2. Armor Special Effects and Abilities
+    public static final ForgeConfigSpec.BooleanValue speedIVibraniumArmor;
+    public static final ForgeConfigSpec.BooleanValue jumpIVibraniumArmor;
+    public static final ForgeConfigSpec.BooleanValue nightVisionVibraniumArmor;
+    public static final ForgeConfigSpec.BooleanValue canWalkOnPowderedSnowVibranium;
+
+    // 3. Bow Settings
+    public static final ForgeConfigSpec.IntValue enchantmentValueVibraniumBow;
+    public static final ForgeConfigSpec.IntValue damageVibraniumBow;
+    public static final ForgeConfigSpec.IntValue arrowCountVibraniumBow;
+
+    // 4. Tool Tier Base Properties
+    public static final ForgeConfigSpec.IntValue speedVibraniumTier;
+    public static final ForgeConfigSpec.IntValue attackDamageBonusVibraniumTier;
+    public static final ForgeConfigSpec.IntValue enchantmentValueVibraniumTier;
+    public static final ForgeConfigSpec.IntValue durabilityVibranium;
+    public static final ForgeConfigSpec.BooleanValue unbreakableVibranium;
+
+    // 5. Pickaxe Stats
+    public static final ForgeConfigSpec.IntValue attackDamageVibraniumPickaxe;
+    public static final ForgeConfigSpec.DoubleValue attackSpeedVibraniumPickaxe;
+
+    // 6. Axe Stats
+    public static final ForgeConfigSpec.IntValue attackDamageVibraniumAxe;
+    public static final ForgeConfigSpec.DoubleValue attackSpeedVibraniumAxe;
+
+    // 7. Shovel Stats
+    public static final ForgeConfigSpec.IntValue attackDamageVibraniumShovel;
+    public static final ForgeConfigSpec.DoubleValue attackSpeedVibraniumShovel;
+
+    // 8. Sword Stats
+    public static final ForgeConfigSpec.IntValue attackDamageVibraniumSword;
+    public static final ForgeConfigSpec.DoubleValue attackSpeedVibraniumSword;
+
+    // 9. Hoe Stats
+    public static final ForgeConfigSpec.IntValue attackDamageVibraniumHoe;
+    public static final ForgeConfigSpec.DoubleValue attackSpeedVibraniumHoe;
+
+    // 10. Paxel Stats
+    public static final ForgeConfigSpec.IntValue attackDamageVibraniumPaxel;
+    public static final ForgeConfigSpec.DoubleValue attackSpeedVibraniumPaxel;
+
+    // 11. Hammer Stats
+    public static final ForgeConfigSpec.IntValue attackDamageVibraniumHammer;
+    public static final ForgeConfigSpec.DoubleValue attackSpeedVibraniumHammer;
+
+    // 12. Excavator Stats
+    public static final ForgeConfigSpec.IntValue attackDamageVibraniumExcavator;
+    public static final ForgeConfigSpec.DoubleValue attackSpeedVibraniumExcavator;
+
+    // 13. Radius (Hammer/Excavator)
+    public static final ForgeConfigSpec.IntValue radiusVibraniumHammer;
+    public static final ForgeConfigSpec.IntValue radiusVibraniumExcavator;
+
+    public static final ForgeConfigSpec SPEC;
+
+    static {
+        BUILDER.push("Armor Protection and Resistance");
+        toughnessValueVibraniumArmor = BUILDER
+                .comment("Toughness value for Vibranium Armor | Default Value = 100")
+                .defineInRange("toughnessValueVibraniumArmor", 100, 0, Integer.MAX_VALUE);
+        enchantmentValueVibraniumArmor = BUILDER
+                .comment("Enchantment value for Vibranium Armor | Default Value = 85")
+                .defineInRange("enchantmentValueVibraniumArmor", 85, 0, Integer.MAX_VALUE);
+        knockbackResistanceValueVibraniumArmor = BUILDER
+                .comment("Knockback resistance for Vibranium Armor | Default Value = 1")
+                .defineInRange("knockbackResistanceValueVibraniumArmor", 1, 0, Integer.MAX_VALUE);
+        protectionValueVibraniumBoots = BUILDER
+                .comment("Protection value for Vibranium Boots | Default Value = 10")
+                .defineInRange("protectionValueVibraniumBoots", 10, 0, Integer.MAX_VALUE);
+        protectionValueVibraniumLeggings = BUILDER
+                .comment("Protection value for Vibranium Leggings | Default Value = 15")
+                .defineInRange("protectionValueVibraniumLeggings", 15, 0, Integer.MAX_VALUE);
+        protectionValueVibraniumChestplate = BUILDER
+                .comment("Protection value for Vibranium Chestplate | Default Value = 20")
+                .defineInRange("protectionValueVibraniumChestplate", 20, 0, Integer.MAX_VALUE);
+        protectionValueVibraniumHelmet = BUILDER
+                .comment("Protection value for Vibranium Helmet | Default Value = 10")
+                .defineInRange("protectionValueVibraniumHelmet", 10, 0, Integer.MAX_VALUE);
+        BUILDER.pop();
+
+        BUILDER.push("Armor Special Effects and Abilities");
+        speedIVibraniumArmor = BUILDER
+                .comment("Whether Vibranium Armor gives Speed I | Default Value = true")
+                .define("speedIVibraniumArmor", true);
+        jumpIVibraniumArmor = BUILDER
+                .comment("Whether Vibranium Armor gives Jump I | Default Value = true")
+                .define("jumpIVibraniumArmor", true);
+        nightVisionVibraniumArmor = BUILDER
+                .comment("Whether Vibranium Armor gives Night Vision | Default Value = true")
+                .define("nightVisionVibraniumArmor", true);
+        canWalkOnPowderedSnowVibranium = BUILDER
+                .comment("Whether Vibranium Armor allows walking on Powdered Snow | Default Value = true")
+                .define("canWalkOnPowderedSnowVibranium", true);
+        BUILDER.pop();
+
+        BUILDER.push("Bow Settings");
+        enchantmentValueVibraniumBow = BUILDER
+                .comment("Enchantment value for Vibranium Bow | Default Value = 5")
+                .defineInRange("enchantmentValueVibraniumBow", 5, 0, Integer.MAX_VALUE);
+        damageVibraniumBow = BUILDER
+                .comment("Damage of Vibranium Bow | Default Value = 8")
+                .defineInRange("damageVibraniumBow", 8, 0, Integer.MAX_VALUE);
+        arrowCountVibraniumBow = BUILDER
+                .comment("Arrow count for Vibranium Bow | Default = 2")
+                .defineInRange("arrowCountVibraniumBow", 2, 0, Integer.MAX_VALUE);
+        BUILDER.pop();
+
+        BUILDER.push("Tool Tier Base Properties");
+        speedVibraniumTier = BUILDER
+                .comment("Speed value of Vibranium Tool Tier | Default Value = 40")
+                .defineInRange("speedVibraniumTier", 40, 0, Integer.MAX_VALUE);
+        attackDamageBonusVibraniumTier = BUILDER
+                .comment("Bonus attack damage for Vibranium Tool Tier | Default Value = 11")
+                .defineInRange("attackDamageBonusVibraniumTier", 11, 0, Integer.MAX_VALUE);
+        enchantmentValueVibraniumTier = BUILDER
+                .comment("Enchantment value for Vibranium Tool Tier | Default Value = 85")
+                .defineInRange("enchantmentValueVibraniumTier", 85, 0, Integer.MAX_VALUE);
+        durabilityVibranium = BUILDER
+                .comment("Durability of Vibranium tools | Default Value = 4062")
+                .defineInRange("durabilityVibranium", 4062, 0, Integer.MAX_VALUE);
+        unbreakableVibranium = BUILDER
+                .comment("Whether Vibranium is unbreakable | Default Value = false")
+                .define("unbreakableVibranium", false);
+        BUILDER.pop();
+
+        BUILDER.push("Pickaxe Stats");
+        attackDamageVibraniumPickaxe = BUILDER
+                .comment("Attack damage of Vibranium Pickaxe | Default Value = 9")
+                .defineInRange("attackDamageVibraniumPickaxe", 9, 0, Integer.MAX_VALUE);
+        attackSpeedVibraniumPickaxe = BUILDER
+                .comment("Attack speed of Vibranium Pickaxe | Default Value = 2.0")
+                .defineInRange("attackSpeedVibraniumPickaxe", 2.0, 0.0, 1024.0);
+        BUILDER.pop();
+
+        BUILDER.push("Axe Stats");
+        attackDamageVibraniumAxe = BUILDER
+                .comment("Attack damage of Vibranium Axe | Default Value = 10")
+                .defineInRange("attackDamageVibraniumAxe", 10, 0, Integer.MAX_VALUE);
+        attackSpeedVibraniumAxe = BUILDER
+                .comment("Attack speed of Vibranium Axe | Default Value = 2.0")
+                .defineInRange("attackSpeedVibraniumAxe", 2.0, 0.0, 1024.0);
+        BUILDER.pop();
+
+        BUILDER.push("Shovel Stats");
+        attackDamageVibraniumShovel = BUILDER
+                .comment("Attack damage of Vibranium Shovel | Default Value = 9")
+                .defineInRange("attackDamageVibraniumShovel", 9, 0, Integer.MAX_VALUE);
+        attackSpeedVibraniumShovel = BUILDER
+                .comment("Attack speed of Vibranium Shovel | Default Value = 2.0")
+                .defineInRange("attackSpeedVibraniumShovel", 2.0, 0.0, 1024.0);
+        BUILDER.pop();
+
+        BUILDER.push("Sword Stats");
+        attackDamageVibraniumSword = BUILDER
+                .comment("Attack damage of Vibranium Sword | Default Value = 10")
+                .defineInRange("attackDamageVibraniumSword", 10, 0, Integer.MAX_VALUE);
+        attackSpeedVibraniumSword = BUILDER
+                .comment("Attack speed of Vibranium Sword | Default Value = 2.0")
+                .defineInRange("attackSpeedVibraniumSword", 2.0, 0.0, 1024.0);
+        BUILDER.pop();
+
+        BUILDER.push("Hoe Stats");
+        attackDamageVibraniumHoe = BUILDER
+                .comment("Attack damage of Vibranium Hoe | Default Value = 7")
+                .defineInRange("attackDamageVibraniumHoe", 7, 0, Integer.MAX_VALUE);
+        attackSpeedVibraniumHoe = BUILDER
+                .comment("Attack speed of Vibranium Hoe | Default Value = 2.0")
+                .defineInRange("attackSpeedVibraniumHoe", 2.0, 0.0, 1024.0);
+        BUILDER.pop();
+
+        BUILDER.push("Paxel Stats");
+        attackDamageVibraniumPaxel = BUILDER
+                .comment("Attack damage of Vibranium Paxel | Default Value = 15")
+                .defineInRange("attackDamageVibraniumPaxel", 15, 0, Integer.MAX_VALUE);
+        attackSpeedVibraniumPaxel = BUILDER
+                .comment("Attack speed of Vibranium Paxel | Default Value = 2.0")
+                .defineInRange("attackSpeedVibraniumPaxel", 2.0, 0.0, 1024.0);
+        BUILDER.pop();
+
+        BUILDER.push("Hammer Stats");
+        attackDamageVibraniumHammer = BUILDER
+                .comment("Attack damage of Vibranium Hammer | Default Value = 13")
+                .defineInRange("attackDamageVibraniumHammer", 13, 0, Integer.MAX_VALUE);
+        attackSpeedVibraniumHammer = BUILDER
+                .comment("Attack speed of Vibranium Hammer | Default Value = 2.0")
+                .defineInRange("attackSpeedVibraniumHammer", 2.0, 0.0, 1024.0);
+        radiusVibraniumHammer = BUILDER
+                .comment("Hammering radius for Vibranium Hammer | Default = 1")
+                .defineInRange("radiusVibraniumHammer", 1, 0, Integer.MAX_VALUE);
+        BUILDER.pop();
+
+        BUILDER.push("Excavator Stats");
+        attackDamageVibraniumExcavator = BUILDER
+                .comment("Attack damage of Vibranium Excavator | Default Value = 13")
+                .defineInRange("attackDamageVibraniumExcavator", 13, 0, Integer.MAX_VALUE);
+        attackSpeedVibraniumExcavator = BUILDER
+                .comment("Attack speed of Vibranium Excavator | Default Value = 2.0")
+                .defineInRange("attackSpeedVibraniumExcavator", 2.0, 0.0, 1024.0);
+        radiusVibraniumExcavator = BUILDER
+                .comment("Excavation radius for Vibranium Excavator | Default = 1")
+                .defineInRange("radiusVibraniumExcavator", 1, 0, Integer.MAX_VALUE);
+        BUILDER.pop();
+
+        SPEC = BUILDER.build();
+    }
+
+    public static void loadConfig(ForgeConfigSpec spec, Path path) {
+        try {
+            Files.createDirectories(path.getParent());
+
+            final CommentedFileConfig configData = CommentedFileConfig.builder(path)
+                    .autosave()
+                    .sync()
+                    .writingMode(WritingMode.REPLACE)
+                    .build();
+
+            configData.load();
+            spec.setConfig(configData);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config file: " + path, e);
+        }
+    }
+}
---
+++ src/main/java/com/blocklegend001/immersiveores/item/custom/vulpus/VulpusArmor.java
@@ -0,0 +1,190 @@
+package com.blocklegend001.immersiveores.item.custom.vulpus;
+
+import com.blocklegend001.immersiveores.config.VulpusConfig;
+import com.blocklegend001.immersiveores.item.ModItems;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.Unit;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
+import net.minecraft.world.item.equipment.ArmorMaterial;
+import net.minecraft.world.item.equipment.ArmorType;
+import net.minecraft.world.item.equipment.Equippable;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
+
+public class VulpusArmor extends Item {
+
+    public VulpusArmor(ArmorMaterial material, ArmorType type, Properties settings) {
+        super(computeSettings(material, type, VulpusConfig.unbreakableVulpus.get(), VulpusConfig.durabilityVulpus.get(), settings));
+    }
+
+    private static Properties computeSettings(ArmorMaterial material, ArmorType type, boolean unbreakable, int durability, Properties settings) {
+        settings.humanoidArmor(material, ArmorType.BODY)
+                .attributes(material.createAttributes(type))
+                .enchantable(material.enchantmentValue())
+                .component(DataComponents.EQUIPPABLE, Equippable.builder(type.getSlot()).setEquipSound(material.equipSound()).setAsset(material.assetId()).build())
+                .durability(durability);
+
+        if (unbreakable) {
+            settings.component(DataComponents.UNBREAKABLE, Unit.INSTANCE);
+        }
+        return settings;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, @Nullable EquipmentSlot slot, int slotIndex) {
+        super.inventoryTick(stack, level, entity, slot, slotIndex);
+        if (!level.isClientSide()) {
+            if (entity instanceof Player player) {
+                if (player.getItemBySlot(EquipmentSlot.FEET).getItem() == ModItems.VULPUS_BOOTS.get()) {
+                    if (VulpusConfig.speedIIVulpusArmor.get()) {
+                        player.addEffect(new MobEffectInstance(MobEffects.SPEED, 400, 1, false, false));
+                    }
+                    if (VulpusConfig.jumpIIVulpusArmor.get()) {
+                        player.addEffect(new MobEffectInstance(MobEffects.JUMP_BOOST, 400, 1, false, false));
+                    }
+                    if (VulpusConfig.fireResistanceVulpusArmor.get()) {
+                        player.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 400, 0, false, false));
+                    }
+                    if (!player.onGround() && player.fallDistance >= 1.0F && VulpusConfig.immuneToFallDamageVulpusArmor.get()) {
+                        player.fallDistance = 0F;
+                    }
+                }
+                if (player.getItemBySlot(EquipmentSlot.CHEST).getItem() == ModItems.VULPUS_CHESTPLATE.get()) {
+                    if (VulpusConfig.fireResistanceVulpusArmor.get()) {
+                        player.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 400, 0, false, false));
+                    }
+                }
+                if (player.getItemBySlot(EquipmentSlot.HEAD).getItem() == ModItems.VULPUS_HELMET.get()) {
+                    if (VulpusConfig.nightVisionVulpusArmor.get()) {
+                        player.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0, false, false));
+                    }
+                    if (VulpusConfig.fireResistanceVulpusArmor.get()) {
+                        player.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 400, 0, false, false));
+                    }
+                }
+                if (player.getItemBySlot(EquipmentSlot.LEGS).getItem() == ModItems.VULPUS_LEGGINGS.get()) {
+                    if (VulpusConfig.fireResistanceVulpusArmor.get()) {
+                        player.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 400, 0, false, false));
+                    }
+                    if (VulpusConfig.neverLoseHungerVulpusArmor.get()) {
+                        player.addEffect(new MobEffectInstance(MobEffects.SATURATION, 400, 99, false, false));
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onCraftedBy(ItemStack stack, Player player) {
+        super.onCraftedBy(stack, player);
+
+        Item resultItem = stack.getItem();
+
+        for (int i = 1; i <= 9; i++) {
+            ItemStack ingredient = player.containerMenu.getSlot(i).getItem();
+            Item ingredientItem = ingredient.getItem();
+
+            if (resultItem == ModItems.VULPUS_HELMET.get() && ingredientItem == ModItems.VIBRANIUM_HELMET.get()
+                    || resultItem == ModItems.VULPUS_CHESTPLATE.get() && ingredientItem == ModItems.VIBRANIUM_CHESTPLATE.get()
+                    || resultItem == ModItems.VULPUS_LEGGINGS.get() && ingredientItem == ModItems.VIBRANIUM_LEGGINGS.get()
+                    || resultItem == ModItems.VULPUS_BOOTS.get() && ingredientItem == ModItems.VIBRANIUM_BOOTS.get()) {
+
+                var ench = ingredient.get(DataComponents.ENCHANTMENTS);
+                if (ench != null) {
+                    stack.set(DataComponents.ENCHANTMENTS, ench);
+                }
+
+                var customName = ingredient.get(DataComponents.CUSTOM_NAME);
+                if (customName != null) {
+                    stack.set(DataComponents.CUSTOM_NAME, customName);
+                }
+
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, TooltipContext p_333372_, TooltipDisplay p_396484_, Consumer<Component> consumer, TooltipFlag p_41424_) {
+        super.appendHoverText(pStack, p_333372_, p_396484_, consumer, p_41424_);
+        if(Screen.hasShiftDown()){
+            if (ModItems.VULPUS_BOOTS.get() == pStack.getItem()) {
+                if (VulpusConfig.unbreakableVulpus.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.unbreakble.tooltip").withStyle(ChatFormatting.RED));
+                }
+                consumer.accept(Component.translatable("tooltip.immersiveores.immunetofire.tooltip").withStyle(ChatFormatting.RED));
+                if (VulpusConfig.speedIIVulpusArmor.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.speed2.tooltip").withStyle(ChatFormatting.RED));
+                } if (VulpusConfig.jumpIIVulpusArmor.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.jump2.tooltip").withStyle(ChatFormatting.RED));
+                }
+                if (VulpusConfig.canWalkOnPowderedSnowVulpus.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.canwalkonpowderedsnow.tooltip").withStyle(ChatFormatting.RED));
+                } if (VulpusConfig.makesPiglinsNeutralVulpus.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.immunetopiglin.tooltip").withStyle(ChatFormatting.RED));
+                } if (VulpusConfig.fireResistanceVulpusArmor.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.playerimmunetofire.tooltip").withStyle(ChatFormatting.RED));
+                } if (VulpusConfig.immuneToFallDamageVulpusArmor.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.immunetofalldamage.tooltip").withStyle(ChatFormatting.RED));
+                }
+            }
+            if (ModItems.VULPUS_CHESTPLATE.get() == pStack.getItem()) {
+                consumer.accept(Component.translatable("tooltip.immersiveores.unbreakble.tooltip").withStyle(ChatFormatting.RED));
+                consumer.accept(Component.translatable("tooltip.immersiveores.immunetofire.tooltip").withStyle(ChatFormatting.RED));
+                if (VulpusConfig.makesPiglinsNeutralVulpus.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.immunetopiglin.tooltip").withStyle(ChatFormatting.RED));
+                } if (VulpusConfig.fireResistanceVulpusArmor.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.playerimmunetofire.tooltip").withStyle(ChatFormatting.RED));
+                }
+            }
+            if (ModItems.VULPUS_HELMET.get() == pStack.getItem()) {
+                consumer.accept(Component.translatable("tooltip.immersiveores.unbreakble.tooltip").withStyle(ChatFormatting.RED));
+                consumer.accept(Component.translatable("tooltip.immersiveores.immunetofire.tooltip").withStyle(ChatFormatting.RED));
+                if (VulpusConfig.nightVisionVulpusArmor.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.nightvision.tooltip").withStyle(ChatFormatting.RED));
+                } if (VulpusConfig.makesPiglinsNeutralVulpus.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.immunetopiglin.tooltip").withStyle(ChatFormatting.RED));
+                } if (VulpusConfig.fireResistanceVulpusArmor.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.playerimmunetofire.tooltip").withStyle(ChatFormatting.RED));
+                }
+            }
+            if (ModItems.VULPUS_LEGGINGS.get() == pStack.getItem()) {
+                consumer.accept(Component.translatable("tooltip.immersiveores.unbreakble.tooltip").withStyle(ChatFormatting.RED));
+                consumer.accept(Component.translatable("tooltip.immersiveores.immunetofire.tooltip").withStyle(ChatFormatting.RED));
+                if (VulpusConfig.makesPiglinsNeutralVulpus.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.immunetopiglin.tooltip").withStyle(ChatFormatting.RED));
+                } if (VulpusConfig.fireResistanceVulpusArmor.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.playerimmunetofire.tooltip").withStyle(ChatFormatting.RED));
+                } if (VulpusConfig.neverLoseHungerVulpusArmor.get()) {
+                    consumer.accept(Component.translatable("tooltip.immersiveores.nerverlosehunger.tooltip").withStyle(ChatFormatting.RED));
+                }
+            }
+        } else {
+            consumer.accept(Component.translatable("tooltip.immersiveores.pressshiftformoreinfo.tooltip").withStyle(ChatFormatting.RED));
+        }
+    }
+
+    public boolean makesPiglinsNeutral(ItemStack stack, LivingEntity wearer) {
+        return VulpusConfig.canWalkOnPowderedSnowVulpus.get();
+    }
+
+    @Override
+    public boolean canWalkOnPowderedSnow(ItemStack stack, LivingEntity wearer) {
+        return VulpusConfig.makesPiglinsNeutralVulpus.get();
+    }
+
+}
