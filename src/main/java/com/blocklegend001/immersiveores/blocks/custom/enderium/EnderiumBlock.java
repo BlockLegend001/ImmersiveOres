81f11eb3bc9ec01fa063d1100d722f172cbed5b5
BlockLegend001 <rosarioarcamone@gmail.com>
2025-08-07 11:46:07 +0200

    Added some missed tooltips to blocks

--- src/main/java/com/blocklegend001/immersiveores/blocks/custom/enderium/EnderiumBlock.java
+++ src/main/java/com/blocklegend001/immersiveores/blocks/custom/enderium/EnderiumBlock.java
@@ -2,6 +2,7 @@ package com.blocklegend001.immersiveores.blocks.custom.enderium;
 
 import com.blocklegend001.immersiveores.blocks.ModBlocks;
 import com.blocklegend001.immersiveores.item.ModItems;
+import com.blocklegend001.immersiveores.tooltip.TooltipBlock;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -15,20 +16,10 @@ import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.List;
 
-public class EnderiumBlock extends Block {
+public class EnderiumBlock extends Block implements TooltipBlock {
     public EnderiumBlock(Properties p_49795_) {
         super(p_49795_);
     }
-
-//    @Override
-//    public void appendHoverText(ItemStack pStack, Item.TooltipContext pContext, List<Component> components, TooltipFlag pTooltipFlag) {
-//
-//        {
-//            components.add(Component.translatable("tooltip.immersiveores.breakenderium.tooltip").withStyle(ChatFormatting.DARK_AQUA));
-//        }
-//
-//        super.appendHoverText(pStack, pContext, components, pTooltipFlag);
-//    }
 
     @Override
     public boolean canHarvestBlock(BlockState state, BlockGetter level, BlockPos pos, Player player) {
@@ -43,5 +34,10 @@ public class EnderiumBlock extends Block {
                     || itemInHand.is(ModItems.ENDERIUM_PAXEL.get());
         }
         return super.canHarvestBlock(state, level, pos, player);
+    }
+
+    @Override
+    public void appendClientTooltip(ItemStack stack, TooltipAccept tooltips) {
+        tooltips.accept(Component.translatable("tooltip.immersiveores.breakenderium.tooltip").withStyle(ChatFormatting.DARK_AQUA));
     }
 }
---
+++ src/main/java/com/blocklegend001/immersiveores/tooltip/TooltipBlockItem.java
@@ -0,0 +1,29 @@
+package com.blocklegend001.immersiveores.tooltip;
+
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Represents a block item that has a tooltip.<br>
+ * Basically redirects the tooltip to the corresponding block extending {@link TooltipItem} in the registry.
+ */
+public class TooltipBlockItem extends BlockItem implements TooltipItem {
+    public TooltipBlockItem(TooltipBlock block, Properties settings) {
+        super((Block) block, settings);
+    }
+
+    @Override
+    public void appendClientTooltip(ItemStack stack, TooltipAccept tooltips) {
+        ResourceLocation value = stack.getItem().components().get(DataComponents.ITEM_MODEL);
+        @Nullable Block block = ForgeRegistries.BLOCKS.getValue(value);
+
+        if(block instanceof TooltipItem tooltipItem) {
+            tooltipItem.appendClientTooltip(stack, tooltips);
+        }
+    }
+}
