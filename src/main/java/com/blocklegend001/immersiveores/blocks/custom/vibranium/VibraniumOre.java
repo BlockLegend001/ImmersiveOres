e06ea1707b509b36196380e7adbd5b3af5961ac1
BlockLegend001 <rosarioarcamone@gmail.com>
2025-08-07 10:35:19 +0200

    Added some missed tooltips to blocks

--- src/main/java/com/blocklegend001/immersiveores/blocks/custom/vibranium/VibraniumOre.java
+++ src/main/java/com/blocklegend001/immersiveores/blocks/custom/vibranium/VibraniumOre.java
@@ -1,5 +1,6 @@
 package com.blocklegend001.immersiveores.blocks.custom.vibranium;
 
+import com.blocklegend001.immersiveores.tooltip.TooltipBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,20 +10,15 @@ import net.minecraft.util.Formatting;
 
 import java.util.List;
 
-public class VibraniumOre extends Block {
-
+public class VibraniumOre extends Block implements TooltipBlock {
     public VibraniumOre(Settings arg) {
         super(arg);
     }
 
-//    @Override
-//    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
-//        {
-//            tooltip.add(Text.translatable("tooltip.immersiveores.vibraniumore.tooltip").formatted(Formatting.LIGHT_PURPLE));
-//            tooltip.add(Text.translatable("tooltip.immersiveores.vibraniumorelevel.tooltip").formatted(Formatting.LIGHT_PURPLE));
-//            tooltip.add(Text.translatable("tooltip.immersiveores.breakvibranium.tooltip").formatted(Formatting.LIGHT_PURPLE));
-//        }
-//
-//        super.appendTooltip(stack, context, tooltip, options);
-//    }
+    @Override
+    public void appendClientTooltip(ItemStack stack, TooltipAccept tooltips) {
+        tooltips.accept(Text.translatable("tooltip.immersiveores.vibraniumore.tooltip").formatted(Formatting.LIGHT_PURPLE));
+        tooltips.accept(Text.translatable("tooltip.immersiveores.vibraniumorelevel.tooltip").formatted(Formatting.LIGHT_PURPLE));
+        tooltips.accept(Text.translatable("tooltip.immersiveores.breakvibranium.tooltip").formatted(Formatting.LIGHT_PURPLE));
+    }
 }
