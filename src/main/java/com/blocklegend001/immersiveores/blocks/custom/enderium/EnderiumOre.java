6f104d9c7974c546ed59f7e1ee2f45e34665eb82
BlockLegend001 <rosarioarcamone@gmail.com>
2025-08-07 10:33:09 +0200

    Added some missed tooltips to blocks

--- src/main/java/com/blocklegend001/immersiveores/blocks/custom/enderium/EnderiumOre.java
+++ src/main/java/com/blocklegend001/immersiveores/blocks/custom/enderium/EnderiumOre.java
@@ -1,17 +1,20 @@
 package com.blocklegend001.immersiveores.blocks.custom.enderium;
 
+import com.blocklegend001.immersiveores.tooltip.TooltipBlock;
 import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
-public class EnderiumOre extends Block {
-
+public class EnderiumOre extends Block implements TooltipBlock {
     public EnderiumOre(Settings settings) {
         super(settings);
     }
 
-//    @Override
-//    public void appendCustomTooltip(ItemStack stack, Item.TooltipContext context, TooltipDisplayComponent displayComponent, Consumer<Text> textConsumer, TooltipType type) {
-//        textConsumer.accept(Text.translatable("tooltip.immersiveores.enderiumore.tooltip").formatted(Formatting.DARK_AQUA));
-//        textConsumer.accept(Text.translatable("tooltip.immersiveores.enderiumorelevel.tooltip").formatted(Formatting.DARK_AQUA));
-//        textConsumer.accept(Text.translatable("tooltip.immersiveores.breakenderium.tooltip").formatted(Formatting.DARK_AQUA));
-//    }
+    @Override
+    public void appendClientTooltip(ItemStack stack, TooltipAccept tooltips) {
+        tooltips.accept(Text.translatable("tooltip.immersiveores.enderiumore.tooltip").formatted(Formatting.DARK_AQUA));
+        tooltips.accept(Text.translatable("tooltip.immersiveores.enderiumorelevel.tooltip").formatted(Formatting.DARK_AQUA));
+        tooltips.accept(Text.translatable("tooltip.immersiveores.breakenderium.tooltip").formatted(Formatting.DARK_AQUA));
+    }
 }
