8eb343e387e42fdbf490a064a130e06ab2cd3077
BlockLegend001 <rosarioarcamone@gmail.com>
2025-08-13 14:19:14 +0200

    Now if you craft any type of stick you will receive 2 instead of 1

--- src/main/java/com/blocklegend001/immersiveores/datagen/ModRecipeProvider.java
+++ src/main/java/com/blocklegend001/immersiveores/datagen/ModRecipeProvider.java
@@ -127,7 +127,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion("has_vibranium_ingot", conditionsFromItem(ModItems.VIBRANIUM_INGOT))
                 .offerTo(exporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.VIBRANIUM_STICK)
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.VIBRANIUM_STICK, 2)
                 .input('E', ModItems.VIBRANIUM_INGOT)
                 .pattern("   ")
                 .pattern(" E ")
@@ -312,7 +312,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion("has_vulpus_ingot", conditionsFromItem(ModItems.VULPUS_INGOT))
                 .offerTo(exporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.VULPUS_STICK)
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.VULPUS_STICK, 2)
                 .input('E', ModItems.VULPUS_INGOT)
                 .pattern("   ")
                 .pattern(" E ")
@@ -453,7 +453,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion("has_enderium_ingot", conditionsFromItem(ModItems.ENDERIUM_INGOT))
                 .offerTo(exporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.ENDERIUM_STICK)
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.ENDERIUM_STICK, 2)
                 .input('E', ModItems.ENDERIUM_INGOT)
                 .pattern("   ")
                 .pattern(" E ")
