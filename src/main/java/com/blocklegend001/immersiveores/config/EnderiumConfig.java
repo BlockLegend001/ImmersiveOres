d4eec3f661c18bc9b80df1d555f8a1046bddb6ef
BlockLegend001 <rosarioarcamone@gmail.com>
2025-08-18 16:34:59 +0200

    Balanced the config

--- src/main/java/com/blocklegend001/immersiveores/config/EnderiumConfig.java
+++ src/main/java/com/blocklegend001/immersiveores/config/EnderiumConfig.java
@@ -53,31 +53,28 @@ public class EnderiumConfig {
     public static int arrowCountEnderiumBow;
     public static int durabilityEnderium;
     public static boolean unbreakableEnderium;
-    public static double attackRangeEnderiumSword;
-    public static double blockRangeEnderiumTool;
-
 
     public static void registerConfigs() {
         configs = new ModConfigProvider();
         createConfigs();
 
         ENDERIUM_CONFIG = SimpleConfig.of("enderium-common.toml")
-                .provider(configs)
-                .request();
+ .provider(configs)
+ .request();
 
         assignConfigs();
     }
 
     private static void createConfigs() {
         configs.addComment("Enderium Armor Protection and Resistance");
-        configs.addKeyValuePair(new Pair<>("protectionValueEnderiumBoots", 30), "Protection value of Enderium Boots");
-        configs.addKeyValuePair(new Pair<>("protectionValueEnderiumLeggings", 50), "Protection value of Enderium Leggings");
-        configs.addKeyValuePair(new Pair<>("protectionValueEnderiumChestplate", 70), "Protection value of Enderium Chestplate");
-        configs.addKeyValuePair(new Pair<>("protectionValueEnderiumHelmet", 30), "Protection value of Enderium Helmet");
+        configs.addKeyValuePair(new Pair<>("protectionValueEnderiumBoots", 12), "Protection value of Enderium Boots");
+        configs.addKeyValuePair(new Pair<>("protectionValueEnderiumLeggings", 16), "Protection value of Enderium Leggings");
+        configs.addKeyValuePair(new Pair<>("protectionValueEnderiumChestplate", 20), "Protection value of Enderium Chestplate");
+        configs.addKeyValuePair(new Pair<>("protectionValueEnderiumHelmet", 12), "Protection value of Enderium Helmet");
 
-        configs.addKeyValuePair(new Pair<>("toughnessValueEnderiumArmor", 200), "Toughness value of Enderium Armor");
+        configs.addKeyValuePair(new Pair<>("toughnessValueEnderiumArmor", 10), "Toughness value of Enderium Armor");
         configs.addKeyValuePair(new Pair<>("knockbackResistanceValueEnderiumArmor", 3), "Knockback resistance of Enderium Armor");
-        configs.addKeyValuePair(new Pair<>("enchantmentValueEnderiumArmor", 85), "Enchantment value of Enderium Armor");
+        configs.addKeyValuePair(new Pair<>("enchantmentValueEnderiumArmor", 65), "Enchantment value of Enderium Armor");
 
         configs.addComment("Enderium Armor Special Effects and Abilities");
         configs.addKeyValuePair(new Pair<>("speedIIIEnderiumArmor", true), "Speed III effect for Enderium Armor");
@@ -97,56 +94,58 @@ public class EnderiumConfig {
         configs.addKeyValuePair(new Pair<>("arrowCountEnderiumBow", 4), "Arrow count for Enderium Bow");
 
         configs.addComment("Enderium Tool Tier Base Properties");
-        configs.addKeyValuePair(new Pair<>("speedEnderiumTier", 130), "Speed value of Enderium tier");
-        configs.addKeyValuePair(new Pair<>("enchantmentValueEnderiumTier", 85), "Enchantment value of Enderium tier");
-        configs.addKeyValuePair(new Pair<>("attackDamageBonusEnderiumTier", 40), "Attack damage bonus of Enderium tier");
-        configs.addKeyValuePair(new Pair<>("durabilityEnderium", 9139), "Durability of Enderium tools");
+        configs.addKeyValuePair(new Pair<>("speedEnderiumTier", 50), "Speed value of Enderium tier");
+        configs.addKeyValuePair(new Pair<>("enchantmentValueEnderiumTier", 65), "Enchantment value of Enderium tier");
+        configs.addKeyValuePair(new Pair<>("attackDamageBonusEnderiumTier", 3), "Attack damage bonus of Enderium tier");
+        configs.addKeyValuePair(new Pair<>("durabilityEnderium", 5139), "Durability of Enderium tools");
         configs.addKeyValuePair(new Pair<>("unbreakableEnderium", false), "Whether Enderium tools are unbreakable");
 
         configs.addComment("Enderium Pickaxe Stats");
-        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumPickaxe", 18), "Attack damage of Enderium Pickaxe");
-        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumPickaxe", 2.0), "Attack speed of Enderium Pickaxe");
+        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumPickaxe", 15), "Attack damage of Enderium Pickaxe");
+        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumPickaxe", 1.0), "Attack speed of Enderium Pickaxe");
 
         configs.addComment("Enderium Axe Stats");
-        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumAxe", 20), "Attack damage of Enderium Axe");
-        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumAxe", 2.0), "Attack speed of Enderium Axe");
+        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumAxe", 18), "Attack damage of Enderium Axe");
+        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumAxe", 1.0), "Attack speed of Enderium Axe");
 
         configs.addComment("Enderium Shovel Stats");
-        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumShovel", 17), "Attack damage of Enderium Shovel");
-        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumShovel", 2.0), "Attack speed of Enderium Shovel");
+        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumShovel", 15), "Attack damage of Enderium Shovel");
+        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumShovel", 1.0), "Attack speed of Enderium Shovel");
 
         configs.addComment("Enderium Sword Stats");
         configs.addKeyValuePair(new Pair<>("attackDamageEnderiumSword", 22), "Attack damage of Enderium Sword");
-        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumSword", 2.0), "Attack speed of Enderium Sword");
+        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumSword", 1.0), "Attack speed of Enderium Sword");
 
         configs.addComment("Enderium Hoe Stats");
-        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumHoe", 14), "Attack damage of Enderium Hoe");
-        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumHoe", 2.0), "Attack speed of Enderium Hoe");
+        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumHoe", 15), "Attack damage of Enderium Hoe");
+        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumHoe", 1.0), "Attack speed of Enderium Hoe");
 
         configs.addComment("Enderium Paxel Stats");
-        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumPaxel", 20), "Attack damage of Enderium Paxel");
-        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumPaxel", 2.0), "Attack speed of Enderium Paxel");
+        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumPaxel", 18), "Attack damage of Enderium Paxel");
+        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumPaxel", 1.0), "Attack speed of Enderium Paxel");
 
         configs.addComment("Enderium Hammer Stats");
-        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumHammer", 20), "Attack damage of Enderium Hammer");
-        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumHammer", 2.0), "Attack speed of Enderium Hammer");
+        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumHammer", 16), "Attack damage of Enderium Hammer");
+        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumHammer", 1.0), "Attack speed of Enderium Hammer");
         configs.addKeyValuePair(new Pair<>("radiusEnderiumHammer", 3), "Hammering radius for Enderium Hammer");
 
         configs.addComment("Enderium Excavator Stats");
-        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumExcavator", 20), "Attack damage of Enderium Excavator");
-        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumExcavator", 2.0), "Attack speed of Enderium Excavator");
+        configs.addKeyValuePair(new Pair<>("attackDamageEnderiumExcavator", 16), "Attack damage of Enderium Excavator");
+        configs.addKeyValuePair(new Pair<>("attackSpeedEnderiumExcavator", 1.0), "Attack speed of Enderium Excavator");
         configs.addKeyValuePair(new Pair<>("radiusEnderiumExcavator", 3), "Excavation radius for Enderium Excavator");
     }
 
     private static void assignConfigs() {
-        protectionValueEnderiumBoots = ENDERIUM_CONFIG.getOrDefault("protectionValueEnderiumBoots", 30);
-        protectionValueEnderiumLeggings = ENDERIUM_CONFIG.getOrDefault("protectionValueEnderiumLeggings", 50);
-        protectionValueEnderiumChestplate = ENDERIUM_CONFIG.getOrDefault("protectionValueEnderiumChestplate", 70);
-        protectionValueEnderiumHelmet = ENDERIUM_CONFIG.getOrDefault("protectionValueEnderiumHelmet", 30);
-        toughnessValueEnderiumArmor = ENDERIUM_CONFIG.getOrDefault("toughnessValueEnderiumArmor", 200);
+        protectionValueEnderiumBoots = ENDERIUM_CONFIG.getOrDefault("protectionValueEnderiumBoots", 12);
+        protectionValueEnderiumLeggings = ENDERIUM_CONFIG.getOrDefault("protectionValueEnderiumLeggings", 16);
+        protectionValueEnderiumChestplate = ENDERIUM_CONFIG.getOrDefault("protectionValueEnderiumChestplate", 20);
+        protectionValueEnderiumHelmet = ENDERIUM_CONFIG.getOrDefault("protectionValueEnderiumHelmet", 12);
+
+        toughnessValueEnderiumArmor = ENDERIUM_CONFIG.getOrDefault("toughnessValueEnderiumArmor", 10);
         knockbackResistanceValueEnderiumArmor = ENDERIUM_CONFIG.getOrDefault("knockbackResistanceValueEnderiumArmor", 3);
-        enchantmentValueEnderiumArmor = ENDERIUM_CONFIG.getOrDefault("enchantmentValueEnderiumArmor", 85);
-        speedIIIEnderiumArmor = ENDERIUM_CONFIG.getOrDefault("speedIIIEnderiumArmor", true);
+        enchantmentValueEnderiumArmor = ENDERIUM_CONFIG.getOrDefault("enchantmentValueEnderiumArmor", 65);
+
+        speedIIIEnderiumArmor= ENDERIUM_CONFIG.getOrDefault("speedIIIEnderiumArmor", true);
         jumpIIIEnderiumArmor = ENDERIUM_CONFIG.getOrDefault("jumpIIIEnderiumArmor", true);
         nightVisionEnderiumArmor = ENDERIUM_CONFIG.getOrDefault("nightVisionEnderiumArmor", true);
         immuneToFallDamageEnderiumArmor = ENDERIUM_CONFIG.getOrDefault("immuneToFallDamageEnderiumArmor", true);
@@ -156,32 +155,43 @@ public class EnderiumConfig {
         canWalkOnPowderedSnowEnderium = ENDERIUM_CONFIG.getOrDefault("canWalkOnPowderedSnowEnderium", true);
         makesPiglinsNeutralEnderium = ENDERIUM_CONFIG.getOrDefault("makesPiglinsNeutralEnderium", true);
         endermanWillNotBeAngryWithYouEnderium = ENDERIUM_CONFIG.getOrDefault("endermanWillNotBeAngryWithYouEnderium", true);
+
         enchantmentValueEnderiumBow = ENDERIUM_CONFIG.getOrDefault("enchantmentValueEnderiumBow", 5);
         damageEnderiumBow = ENDERIUM_CONFIG.getOrDefault("damageEnderiumBow", 14);
         arrowCountEnderiumBow = ENDERIUM_CONFIG.getOrDefault("arrowCountEnderiumBow", 4);
-        speedEnderiumTier = ENDERIUM_CONFIG.getOrDefault("speedEnderiumTier", 130);
-        enchantmentValueEnderiumTier = ENDERIUM_CONFIG.getOrDefault("enchantmentValueEnderiumTier", 85);
-        attackDamageBonusEnderiumTier = ENDERIUM_CONFIG.getOrDefault("attackDamageBonusEnderiumTier", 40);
-        durabilityEnderium = ENDERIUM_CONFIG.getOrDefault("durabilityEnderium", 9139);
+
+        speedEnderiumTier = ENDERIUM_CONFIG.getOrDefault("speedEnderiumTier", 50);
+        enchantmentValueEnderiumTier = ENDERIUM_CONFIG.getOrDefault("enchantmentValueEnderiumTier", 65);
+        attackDamageBonusEnderiumTier = ENDERIUM_CONFIG.getOrDefault("attackDamageBonusEnderiumTier", 3);
+        durabilityEnderium = ENDERIUM_CONFIG.getOrDefault("durabilityEnderium", 5139);
         unbreakableEnderium = ENDERIUM_CONFIG.getOrDefault("unbreakableEnderium", false);
-        attackDamageEnderiumPickaxe = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumPickaxe", 18);
-        attackSpeedEnderiumPickaxe = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumPickaxe", 2.0);
-        attackDamageEnderiumAxe = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumAxe", 20);
-        attackSpeedEnderiumAxe = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumAxe", 2.0);
-        attackDamageEnderiumShovel = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumShovel", 17);
-        attackSpeedEnderiumShovel = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumShovel", 2.0);
+
+        attackDamageEnderiumPickaxe = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumPickaxe", 15);
+        attackSpeedEnderiumPickaxe = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumPickaxe", 1.0);
+
+        attackDamageEnderiumAxe = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumAxe", 18);
+        attackSpeedEnderiumAxe = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumAxe", 1.0);
+
+        attackDamageEnderiumShovel = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumShovel", 15);
+        attackSpeedEnderiumShovel = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumShovel", 1.0);
+
         attackDamageEnderiumSword = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumSword", 22);
-        attackSpeedEnderiumSword = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumSword", 2.0);
-        attackDamageEnderiumHoe = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumHoe", 14);
-        attackSpeedEnderiumHoe = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumHoe", 2.0);
-        attackDamageEnderiumPaxel = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumPaxel", 20);
-        attackSpeedEnderiumPaxel = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumPaxel", 2.0);
-        attackDamageEnderiumHammer = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumHammer", 20);
-        attackSpeedEnderiumHammer = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumHammer", 2.0);
+        attackSpeedEnderiumSword = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumSword", 1.0);
+
+        attackDamageEnderiumHoe = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumHoe", 15);
+        attackSpeedEnderiumHoe = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumHoe", 1.0);
+
+        attackDamageEnderiumPaxel = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumPaxel", 18);
+        attackSpeedEnderiumPaxel = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumPaxel", 1.0);
+
+        attackDamageEnderiumHammer = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumHammer", 16);
+        attackSpeedEnderiumHammer = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumHammer", 1.0);
         radiusEnderiumHammer = ENDERIUM_CONFIG.getOrDefault("radiusEnderiumHammer", 3);
-        attackDamageEnderiumExcavator = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumExcavator", 20);
-        attackSpeedEnderiumExcavator = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumExcavator", 2.0);
+
+        attackDamageEnderiumExcavator = ENDERIUM_CONFIG.getOrDefault("attackDamageEnderiumExcavator", 16);
+        attackSpeedEnderiumExcavator = ENDERIUM_CONFIG.getOrDefault("attackSpeedEnderiumExcavator", 1.0);
         radiusEnderiumExcavator = ENDERIUM_CONFIG.getOrDefault("radiusEnderiumExcavator", 3);
+
 
         System.out.println("All " + configs.getConfigsList().size() + " have been set properly");
     }
--- src/main/java/com/blocklegend001/immersiveores/config/VulpusConfig.java
+++ src/main/java/com/blocklegend001/immersiveores/config/VulpusConfig.java
@@ -51,9 +51,6 @@ public class VulpusConfig {
     public static int arrowCountVulpusBow;
     public static int durabilityVulpus;
     public static boolean unbreakableVulpus;
-    public static double attackRangeVulpusSword;
-    public static double blockRangeVulpusTool;
-
 
     public static void registerConfigs() {
         configs = new ModConfigProvider();
@@ -68,12 +65,12 @@ public class VulpusConfig {
 
     private static void createConfigs() {
         configs.addComment("Vulpus Armor Protection and Resistance");
-        configs.addKeyValuePair(new Pair<>("protectionValueVulpusBoots", 20), "Protection value of Vulpus Boots");
-        configs.addKeyValuePair(new Pair<>("protectionValueVulpusLeggings", 25), "Protection value of Vulpus Leggings");
-        configs.addKeyValuePair(new Pair<>("protectionValueVulpusChestplate", 35), "Protection value of Vulpus Chestplate");
-        configs.addKeyValuePair(new Pair<>("protectionValueVulpusHelmet", 20), "Protection value of Vulpus Helmet");
-        configs.addKeyValuePair(new Pair<>("toughnessValueVulpusArmor", 150), "Toughness value of Vulpus Armor");
-        configs.addKeyValuePair(new Pair<>("enchantmentValueVulpusArmor", 85), "Enchantment value of Vulpus Armor");
+        configs.addKeyValuePair(new Pair<>("protectionValueVulpusBoots", 9), "Protection value of Vulpus Boots");
+        configs.addKeyValuePair(new Pair<>("protectionValueVulpusLeggings", 12), "Protection value of Vulpus Leggings");
+        configs.addKeyValuePair(new Pair<>("protectionValueVulpusChestplate", 16), "Protection value of Vulpus Chestplate");
+        configs.addKeyValuePair(new Pair<>("protectionValueVulpusHelmet", 9), "Protection value of Vulpus Helmet");
+        configs.addKeyValuePair(new Pair<>("toughnessValueVulpusArmor", 7), "Toughness value of Vulpus Armor");
+        configs.addKeyValuePair(new Pair<>("enchantmentValueVulpusArmor", 65), "Enchantment value of Vulpus Armor");
         configs.addKeyValuePair(new Pair<>("knockbackResistanceValueVulpusArmor", 2), "Knockback resistance of Vulpus Armor");
 
         configs.addComment("Vulpus Armor Special Effects and Abilities");
@@ -92,58 +89,57 @@ public class VulpusConfig {
         configs.addKeyValuePair(new Pair<>("arrowCountVulpusBow", 3), "Arrow count for Vulpus Bow");
 
         configs.addComment("Vulpus Tool Tier Base");
-        configs.addKeyValuePair(new Pair<>("speedVulpusTier", 75), "Mining speed of Vulpus tier");
-        configs.addKeyValuePair(new Pair<>("enchantmentValueVulpusTier", 85), "Enchantment value of Vulpus tier");
-        configs.addKeyValuePair(new Pair<>("attackDamageBonusVulpusTier", 20), "Attack damage bonus of Vulpus tier");
-        configs.addKeyValuePair(new Pair<>("durabilityVulpus", 6093), "Durability of Vulpus tools");
+        configs.addKeyValuePair(new Pair<>("speedVulpusTier", 40), "Mining speed of Vulpus tier");
+        configs.addKeyValuePair(new Pair<>("enchantmentValueVulpusTier", 65), "Enchantment value of Vulpus tier");
+        configs.addKeyValuePair(new Pair<>("attackDamageBonusVulpusTier", 3), "Attack damage bonus of Vulpus tier");
+        configs.addKeyValuePair(new Pair<>("durabilityVulpus", 3093), "Durability of Vulpus tools");
         configs.addKeyValuePair(new Pair<>("unbreakableVulpus", false), "Whether Vulpus tools are unbreakable");
 
         configs.addComment("Vulpus Pickaxe");
         configs.addKeyValuePair(new Pair<>("attackDamageVulpusPickaxe", 10), "Attack damage of Vulpus Pickaxe");
-        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusPickaxe", 2.0), "Attack speed of Vulpus Pickaxe");
+        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusPickaxe", 0.5), "Attack speed of Vulpus Pickaxe");
 
         configs.addComment("Vulpus Axe");
         configs.addKeyValuePair(new Pair<>("attackDamageVulpusAxe", 15), "Attack damage of Vulpus Axe");
-        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusAxe", 2.0), "Attack speed of Vulpus Axe");
+        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusAxe", 0.5), "Attack speed of Vulpus Axe");
 
         configs.addComment("Vulpus Shovel");
-        configs.addKeyValuePair(new Pair<>("attackDamageVulpusShovel", 12), "Attack damage of Vulpus Shovel");
-        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusShovel", 2.0), "Attack speed of Vulpus Shovel");
+        configs.addKeyValuePair(new Pair<>("attackDamageVulpusShovel", 10), "Attack damage of Vulpus Shovel");
+        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusShovel", 0.5), "Attack speed of Vulpus Shovel");
 
         configs.addComment("Vulpus Sword");
         configs.addKeyValuePair(new Pair<>("attackDamageVulpusSword", 14), "Attack damage of Vulpus Sword");
-        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusSword", 2.0), "Attack speed of Vulpus Sword");
+        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusSword", 0.5), "Attack speed of Vulpus Sword");
 
         configs.addComment("Vulpus Hoe");
         configs.addKeyValuePair(new Pair<>("attackDamageVulpusHoe", 10), "Attack damage of Vulpus Hoe");
-        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusHoe", 2.0), "Attack speed of Vulpus Hoe");
+        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusHoe", 0.5), "Attack speed of Vulpus Hoe");
 
         configs.addComment("Vulpus Paxel");
-        configs.addKeyValuePair(new Pair<>("attackDamageVulpusPaxel", 18), "Attack damage of Vulpus Paxel");
-        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusPaxel", 2.0), "Attack speed of Vulpus Paxel");
+        configs.addKeyValuePair(new Pair<>("attackDamageVulpusPaxel", 15), "Attack damage of Vulpus Paxel");
+        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusPaxel", 0.5), "Attack speed of Vulpus Paxel");
 
         configs.addComment("Vulpus Hammer");
-        configs.addKeyValuePair(new Pair<>("attackDamageVulpusHammer", 17), "Attack damage of Vulpus Hammer");
-        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusHammer", 2.0), "Attack speed of Vulpus Hammer");
+        configs.addKeyValuePair(new Pair<>("attackDamageVulpusHammer", 11), "Attack damage of Vulpus Hammer");
+        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusHammer", 0.5), "Attack speed of Vulpus Hammer");
         configs.addKeyValuePair(new Pair<>("radiusVulpusHammer", 2), "Hammering radius for Vulpus Hammer");
 
         configs.addComment("Vulpus Excavator");
-        configs.addKeyValuePair(new Pair<>("attackDamageVulpusExcavator", 17), "Attack damage of Vulpus Excavator");
-        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusExcavator", 2.0), "Attack speed of Vulpus Excavator");
+        configs.addKeyValuePair(new Pair<>("attackDamageVulpusExcavator", 11), "Attack damage of Vulpus Excavator");
+        configs.addKeyValuePair(new Pair<>("attackSpeedVulpusExcavator", 0.5), "Attack speed of Vulpus Excavator");
         configs.addKeyValuePair(new Pair<>("radiusVulpusExcavator", 2), "Excavation radius for Vulpus Excavator");
-
     }
 
     private static void assignConfigs() {
-        protectionValueVulpusBoots = VULPUS_CONFIG.getOrDefault("protectionValueVulpusBoots", 20);
-        protectionValueVulpusLeggings = VULPUS_CONFIG.getOrDefault("protectionValueVulpusLeggings", 25);
-        protectionValueVulpusChestplate = VULPUS_CONFIG.getOrDefault("protectionValueVulpusChestplate", 35);
-        protectionValueVulpusHelmet = VULPUS_CONFIG.getOrDefault("protectionValueVulpusHelmet", 20);
+        protectionValueVulpusBoots = VULPUS_CONFIG.getOrDefault("protectionValueVulpusBoots", 9);
+        protectionValueVulpusLeggings = VULPUS_CONFIG.getOrDefault("protectionValueVulpusLeggings", 12);
+        protectionValueVulpusChestplate = VULPUS_CONFIG.getOrDefault("protectionValueVulpusChestplate", 16);
+        protectionValueVulpusHelmet = VULPUS_CONFIG.getOrDefault("protectionValueVulpusHelmet", 9);
 
-        toughnessValueVulpusArmor = VULPUS_CONFIG.getOrDefault("toughnessValueVulpusArmor", 150);
+        toughnessValueVulpusArmor = VULPUS_CONFIG.getOrDefault("toughnessValueVulpusArmor", 7);
         knockbackResistanceValueVulpusArmor = VULPUS_CONFIG.getOrDefault("knockbackResistanceValueVulpusArmor", 2);
-        enchantmentValueVulpusArmor = VULPUS_CONFIG.getOrDefault("enchantmentValueVulpusArmor", 85);
-        durabilityVulpus = VULPUS_CONFIG.getOrDefault("durabilityVulpus", 4324);
+        enchantmentValueVulpusArmor = VULPUS_CONFIG.getOrDefault("enchantmentValueVulpusArmor", 65);
+        durabilityVulpus = VULPUS_CONFIG.getOrDefault("durabilityVulpus", 3093);
         unbreakableVulpus = VULPUS_CONFIG.getOrDefault("unbreakableVulpus", false);
 
         speedIIVulpusArmor = VULPUS_CONFIG.getOrDefault("speedIIVulpusArmor", true);
@@ -155,10 +151,10 @@ public class VulpusConfig {
         canWalkOnPowderedSnowVulpus = VULPUS_CONFIG.getOrDefault("canWalkOnPowderedSnowVulpus", true);
         makesPiglinsNeutralVulpus = VULPUS_CONFIG.getOrDefault("makesPiglinsNeutralVulpus", true);
 
-        speedVulpusTier = VULPUS_CONFIG.getOrDefault("speedVulpusTier", 75);
-        attackDamageBonusVulpusTier = VULPUS_CONFIG.getOrDefault("attackDamageBonusVulpusTier", 20);
-        enchantmentValueVulpusTier = VULPUS_CONFIG.getOrDefault("enchantmentValueVulpusTier", 85);
-        durabilityVulpus = VULPUS_CONFIG.getOrDefault("durabilityVulpus", 6093);
+        speedVulpusTier = VULPUS_CONFIG.getOrDefault("speedVulpusTier", 40);
+        attackDamageBonusVulpusTier = VULPUS_CONFIG.getOrDefault("attackDamageBonusVulpusTier", 3);
+        enchantmentValueVulpusTier = VULPUS_CONFIG.getOrDefault("enchantmentValueVulpusTier", 65);
+        durabilityVulpus = VULPUS_CONFIG.getOrDefault("durabilityVulpus", 3093);
         unbreakableVulpus = VULPUS_CONFIG.getOrDefault("unbreakableVulpus", false);
 
         enchantmentValueVulpusBow = VULPUS_CONFIG.getOrDefault("enchantmentValueVulpusBow", 5);
@@ -166,29 +162,29 @@ public class VulpusConfig {
         arrowCountVulpusBow = VULPUS_CONFIG.getOrDefault("arrowCountVulpusBow", 3);
 
         attackDamageVulpusPickaxe = VULPUS_CONFIG.getOrDefault("attackDamageVulpusPickaxe", 10);
-        attackSpeedVulpusPickaxe = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusPickaxe", 2.0);
+        attackSpeedVulpusPickaxe = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusPickaxe", 0.5);
 
         attackDamageVulpusAxe = VULPUS_CONFIG.getOrDefault("attackDamageVulpusAxe", 15);
-        attackSpeedVulpusAxe = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusAxe", 2.0);
+        attackSpeedVulpusAxe = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusAxe", 0.5);
 
-        attackDamageVulpusShovel = VULPUS_CONFIG.getOrDefault("attackDamageVulpusShovel", 12);
-        attackSpeedVulpusShovel = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusShovel", 2.0);
+        attackDamageVulpusShovel = VULPUS_CONFIG.getOrDefault("attackDamageVulpusShovel", 10);
+        attackSpeedVulpusShovel = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusShovel", 0.5);
 
         attackDamageVulpusSword = VULPUS_CONFIG.getOrDefault("attackDamageVulpusSword", 14);
-        attackSpeedVulpusSword = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusSword", 2.0);
+        attackSpeedVulpusSword = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusSword", 0.5);
 
         attackDamageVulpusHoe = VULPUS_CONFIG.getOrDefault("attackDamageVulpusHoe", 10);
-        attackSpeedVulpusHoe = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusHoe", 2.0);
+        attackSpeedVulpusHoe = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusHoe", 0.5);
 
-        attackDamageVulpusPaxel = VULPUS_CONFIG.getOrDefault("attackDamageVulpusPaxel", 18);
-        attackSpeedVulpusPaxel = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusPaxel", 2.0);
+        attackDamageVulpusPaxel = VULPUS_CONFIG.getOrDefault("attackDamageVulpusPaxel", 15);
+        attackSpeedVulpusPaxel = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusPaxel", 0.5);
 
-        attackDamageVulpusHammer = VULPUS_CONFIG.getOrDefault("attackDamageVulpusHammer", 17);
-        attackSpeedVulpusHammer = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusHammer", 2.0);
+        attackDamageVulpusHammer = VULPUS_CONFIG.getOrDefault("attackDamageVulpusHammer", 11);
+        attackSpeedVulpusHammer = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusHammer", 0.5);
         radiusVulpusHammer = VULPUS_CONFIG.getOrDefault("radiusVulpusHammer", 2);
 
-        attackDamageVulpusExcavator = VULPUS_CONFIG.getOrDefault("attackDamageVulpusExcavator", 17);
-        attackSpeedVulpusExcavator = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusExcavator", 2.0);
+        attackDamageVulpusExcavator = VULPUS_CONFIG.getOrDefault("attackDamageVulpusExcavator", 11);
+        attackSpeedVulpusExcavator = VULPUS_CONFIG.getOrDefault("attackSpeedVulpusExcavator", 0.5);
         radiusVulpusExcavator = VULPUS_CONFIG.getOrDefault("radiusVulpusExcavator", 2);
     }
 
