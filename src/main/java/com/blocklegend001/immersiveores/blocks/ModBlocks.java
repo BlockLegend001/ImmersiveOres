82322deade3516b9f240f02e072e10252956d0a7
BlockLegend001 <rosarioarcamone@gmail.com>
2025-08-22 16:15:54 +0200

    Removed unnecessary color codes from lang files. Colors are now fully managed by code.

--- src/main/java/com/blocklegend001/immersiveores/blocks/ModBlocks.java
+++ src/main/java/com/blocklegend001/immersiveores/blocks/ModBlocks.java
@@ -83,11 +83,6 @@ public class ModBlocks {
         return toReturn;
     }
 
-//    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
-//        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()
-//                .setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(ImmersiveOres.MODID, name)))));
-//    }
-
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
         return ModItems.ITEMS.register(name, () -> {
             Block realBlock = block.get();
