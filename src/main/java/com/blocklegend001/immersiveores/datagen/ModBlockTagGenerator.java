e63ec873f668947da9cd1c5bd107afaa5122cc38
BlockLegend001 <rosarioarcamone@gmail.com>
2025-08-27 22:42:00 +0200

    Fixed some incompatibility issues with Jade & WTHIT

--- src/main/java/com/blocklegend001/immersiveores/datagen/ModBlockTagGenerator.java
+++ src/main/java/com/blocklegend001/immersiveores/datagen/ModBlockTagGenerator.java
@@ -36,7 +36,7 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
                         ModBlocks.RAW_VULPUS_BLOCK.get(),
                         ModBlocks.RAW_ENDERIUM_BLOCK.get());
 
-        this.tag(Tags.Blocks.NEEDS_NETHERITE_TOOL)
+        this.tag(ModTags.Blocks.NEEDS_NETHERITE_TOOL)
                 .add(ModBlocks.VIBRANIUM_BLOCK.get(),
                         ModBlocks.VIBRANIUM_ORE.get(),
                         ModBlocks.RAW_VIBRANIUM_BLOCK.get());
@@ -75,20 +75,83 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
                 .addTag(ModTags.Blocks.NEEDS_ENDERIUM_TOOL);
 
         this.tag(ModTags.Blocks.INCORRECT_FOR_ENDERIUM_TOOL)
-                .addTag(BlockTags.INCORRECT_FOR_NETHERITE_TOOL)
-                .remove(ModTags.Blocks.NEEDS_ENDERIUM_TOOL);
+                .replace(true);
 
         this.tag(ModTags.Blocks.INCORRECT_FOR_VULPUS_TOOL)
-                .addTag(BlockTags.INCORRECT_FOR_NETHERITE_TOOL)
-                .remove(ModTags.Blocks.NEEDS_VULPUS_TOOL);
+                .replace(true);
 
         this.tag(ModTags.Blocks.INCORRECT_FOR_VIBRANIUM_TOOL)
-                .addTag(BlockTags.INCORRECT_FOR_NETHERITE_TOOL)
-                .remove(ModTags.Blocks.NEEDS_VIBRANIUM_TOOL);
+                .add(ModBlocks.ENDERIUM_BLOCK.get(),
+                        ModBlocks.ENDERIUM_ORE.get(),
+                        ModBlocks.RAW_ENDERIUM_BLOCK.get())
+                .replace(true);
 
         this.tag(Tags.Blocks.ORES)
-                .add(ModBlocks.VIBRANIUM_ORE.get())
-                .add(ModBlocks.VULPUS_ORE.get())
-                .add(ModBlocks.ENDERIUM_ORE.get());
+                .add(ModBlocks.VIBRANIUM_ORE.get(),
+                        ModBlocks.VULPUS_ORE.get(),
+                        ModBlocks.ENDERIUM_ORE.get());
+
+        this.tag(BlockTags.INCORRECT_FOR_WOODEN_TOOL)
+                .add(ModBlocks.VIBRANIUM_BLOCK.get(),
+                        ModBlocks.VULPUS_BLOCK.get(),
+                        ModBlocks.ENDERIUM_BLOCK.get(),
+                        ModBlocks.VIBRANIUM_ORE.get(),
+                        ModBlocks.VULPUS_ORE.get(),
+                        ModBlocks.ENDERIUM_ORE.get(),
+                        ModBlocks.RAW_VIBRANIUM_BLOCK.get(),
+                        ModBlocks.RAW_VULPUS_BLOCK.get(),
+                        ModBlocks.RAW_ENDERIUM_BLOCK.get());
+
+        this.tag(BlockTags.INCORRECT_FOR_STONE_TOOL)
+                .add(ModBlocks.VIBRANIUM_BLOCK.get(),
+                        ModBlocks.VULPUS_BLOCK.get(),
+                        ModBlocks.ENDERIUM_BLOCK.get(),
+                        ModBlocks.VIBRANIUM_ORE.get(),
+                        ModBlocks.VULPUS_ORE.get(),
+                        ModBlocks.ENDERIUM_ORE.get(),
+                        ModBlocks.RAW_VIBRANIUM_BLOCK.get(),
+                        ModBlocks.RAW_VULPUS_BLOCK.get(),
+                        ModBlocks.RAW_ENDERIUM_BLOCK.get());
+
+        this.tag(BlockTags.INCORRECT_FOR_IRON_TOOL)
+                .add(ModBlocks.VIBRANIUM_BLOCK.get(),
+                        ModBlocks.VULPUS_BLOCK.get(),
+                        ModBlocks.ENDERIUM_BLOCK.get(),
+                        ModBlocks.VIBRANIUM_ORE.get(),
+                        ModBlocks.VULPUS_ORE.get(),
+                        ModBlocks.ENDERIUM_ORE.get(),
+                        ModBlocks.RAW_VIBRANIUM_BLOCK.get(),
+                        ModBlocks.RAW_VULPUS_BLOCK.get(),
+                        ModBlocks.RAW_ENDERIUM_BLOCK.get());
+
+        this.tag(BlockTags.INCORRECT_FOR_GOLD_TOOL)
+                .add(ModBlocks.VIBRANIUM_BLOCK.get(),
+                        ModBlocks.VULPUS_BLOCK.get(),
+                        ModBlocks.ENDERIUM_BLOCK.get(),
+                        ModBlocks.VIBRANIUM_ORE.get(),
+                        ModBlocks.VULPUS_ORE.get(),
+                        ModBlocks.ENDERIUM_ORE.get(),
+                        ModBlocks.RAW_VIBRANIUM_BLOCK.get(),
+                        ModBlocks.RAW_VULPUS_BLOCK.get(),
+                        ModBlocks.RAW_ENDERIUM_BLOCK.get());
+
+        this.tag(BlockTags.INCORRECT_FOR_DIAMOND_TOOL)
+                .add(ModBlocks.VIBRANIUM_BLOCK.get(),
+                        ModBlocks.VULPUS_BLOCK.get(),
+                        ModBlocks.ENDERIUM_BLOCK.get(),
+                        ModBlocks.VIBRANIUM_ORE.get(),
+                        ModBlocks.VULPUS_ORE.get(),
+                        ModBlocks.ENDERIUM_ORE.get(),
+                        ModBlocks.RAW_VIBRANIUM_BLOCK.get(),
+                        ModBlocks.RAW_VULPUS_BLOCK.get(),
+                        ModBlocks.RAW_ENDERIUM_BLOCK.get());
+
+        this.tag(BlockTags.INCORRECT_FOR_NETHERITE_TOOL)
+                .add(ModBlocks.VULPUS_BLOCK.get(),
+                        ModBlocks.ENDERIUM_BLOCK.get(),
+                        ModBlocks.VULPUS_ORE.get(),
+                        ModBlocks.ENDERIUM_ORE.get(),
+                        ModBlocks.RAW_VULPUS_BLOCK.get(),
+                        ModBlocks.RAW_ENDERIUM_BLOCK.get());
     }
 }
