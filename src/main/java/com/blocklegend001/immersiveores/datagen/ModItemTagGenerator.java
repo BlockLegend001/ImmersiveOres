380981bf8cf272709f4cbe6082c164975e876c39
BlockLegend001 <rosarioarcamone@gmail.com>
2025-09-01 13:19:37 +0200

    Added more tags

--- src/main/java/com/blocklegend001/immersiveores/datagen/ModItemTagGenerator.java
+++ src/main/java/com/blocklegend001/immersiveores/datagen/ModItemTagGenerator.java
@@ -257,5 +257,143 @@ public class ModItemTagGenerator extends FabricTagProvider.ItemTagProvider {
 
         valueLookupBuilder(ModTags.Items.TOOLS_NETHERITE)
                 .add(Items.NETHERITE_PICKAXE);
+
+        valueLookupBuilder(ItemTags.PICKAXES).add(ModItems.VIBRANIUM_PICKAXE,
+                ModItems.VULPUS_PICKAXE,
+                ModItems.ENDERIUM_PICKAXE,
+                ModItems.VIBRANIUM_PAXEL,
+                ModItems.VULPUS_PAXEL,
+                ModItems.ENDERIUM_PAXEL);
+
+        valueLookupBuilder(ItemTags.SHOVELS).add(ModItems.VIBRANIUM_SHOVEL,
+                ModItems.VULPUS_SHOVEL,
+                ModItems.ENDERIUM_SHOVEL,
+                ModItems.VIBRANIUM_PAXEL,
+                ModItems.VULPUS_PAXEL,
+                ModItems.ENDERIUM_PAXEL);
+
+        valueLookupBuilder(ItemTags.AXES).add(ModItems.VIBRANIUM_AXE,
+                ModItems.VULPUS_AXE,
+                ModItems.ENDERIUM_AXE,
+                ModItems.VIBRANIUM_PAXEL,
+                ModItems.VULPUS_PAXEL,
+                ModItems.ENDERIUM_PAXEL);
+
+        valueLookupBuilder(ItemTags.HOES).add(ModItems.VIBRANIUM_HOE,
+                ModItems.VULPUS_HOE,
+                ModItems.ENDERIUM_HOE);
+
+        valueLookupBuilder(ItemTags.SWORDS).add(ModItems.VIBRANIUM_SWORD,
+                ModItems.VULPUS_SWORD,
+                ModItems.ENDERIUM_SWORD);
+
+        valueLookupBuilder(ConventionalItemTags.BOW_TOOLS).add(ModItems.VIBRANIUM_BOW,
+                ModItems.VULPUS_BOW,
+                ModItems.ENDERIUM_BOW);
+
+        valueLookupBuilder(ConventionalItemTags.TOOLS).add(ModItems.VIBRANIUM_HAMMER,
+                ModItems.VULPUS_HAMMER,
+                ModItems.ENDERIUM_HAMMER,
+                ModItems.VIBRANIUM_EXCAVATOR,
+                ModItems.VULPUS_EXCAVATOR,
+                ModItems.ENDERIUM_EXCAVATOR,
+                ModItems.VIBRANIUM_PICKAXE,
+                ModItems.VULPUS_PICKAXE,
+                ModItems.ENDERIUM_PICKAXE,
+                ModItems.VIBRANIUM_HOE,
+                ModItems.VULPUS_HOE,
+                ModItems.ENDERIUM_HOE,
+                ModItems.VIBRANIUM_AXE,
+                ModItems.VULPUS_AXE,
+                ModItems.ENDERIUM_AXE,
+                ModItems.VIBRANIUM_SHOVEL,
+                ModItems.VULPUS_SHOVEL,
+                ModItems.ENDERIUM_SHOVEL,
+                ModItems.VIBRANIUM_PAXEL,
+                ModItems.VULPUS_PAXEL,
+                ModItems.ENDERIUM_PAXEL);
+
+        valueLookupBuilder(ConventionalItemTags.MINING_TOOL_TOOLS).add(ModItems.VIBRANIUM_HAMMER,
+                ModItems.VULPUS_HAMMER,
+                ModItems.ENDERIUM_HAMMER,
+                ModItems.VIBRANIUM_EXCAVATOR,
+                ModItems.VULPUS_EXCAVATOR,
+                ModItems.ENDERIUM_EXCAVATOR,
+                ModItems.VIBRANIUM_PICKAXE,
+                ModItems.VULPUS_PICKAXE,
+                ModItems.ENDERIUM_PICKAXE,
+                ModItems.VIBRANIUM_HOE,
+                ModItems.VULPUS_HOE,
+                ModItems.ENDERIUM_HOE,
+                ModItems.VIBRANIUM_AXE,
+                ModItems.VULPUS_AXE,
+                ModItems.ENDERIUM_AXE,
+                ModItems.VIBRANIUM_SHOVEL,
+                ModItems.VULPUS_SHOVEL,
+                ModItems.ENDERIUM_SHOVEL,
+                ModItems.VIBRANIUM_PAXEL,
+                ModItems.VULPUS_PAXEL,
+                ModItems.ENDERIUM_PAXEL);
+
+        valueLookupBuilder(ItemTags.CHEST_ARMOR).add(ModItems.VIBRANIUM_CHESTPLATE,
+                ModItems.VULPUS_CHESTPLATE,
+                ModItems.ENDERIUM_CHESTPLATE);
+
+        valueLookupBuilder(ItemTags.FOOT_ARMOR).add(ModItems.VIBRANIUM_BOOTS,
+                ModItems.VULPUS_BOOTS,
+                ModItems.ENDERIUM_BOOTS);
+
+        valueLookupBuilder(ItemTags.LEG_ARMOR).add(ModItems.VIBRANIUM_LEGGINGS,
+                ModItems.VULPUS_LEGGINGS,
+                ModItems.ENDERIUM_LEGGINGS);
+
+        valueLookupBuilder(ItemTags.HEAD_ARMOR).add(ModItems.VIBRANIUM_HELMET,
+                ModItems.VULPUS_HELMET,
+                ModItems.ENDERIUM_HELMET);
+
+        valueLookupBuilder(ConventionalItemTags.ARMORS).add(ModItems.VIBRANIUM_HELMET,
+                ModItems.VIBRANIUM_CHESTPLATE,
+                ModItems.VIBRANIUM_LEGGINGS,
+                ModItems.VIBRANIUM_BOOTS,
+                ModItems.VULPUS_HELMET,
+                ModItems.VULPUS_CHESTPLATE,
+                ModItems.VULPUS_LEGGINGS,
+                ModItems.VULPUS_BOOTS,
+                ModItems.ENDERIUM_HELMET,
+                ModItems.ENDERIUM_CHESTPLATE,
+                ModItems.ENDERIUM_LEGGINGS,
+                ModItems.ENDERIUM_BOOTS);
+
+        valueLookupBuilder(ConventionalItemTags.NUGGETS)
+                .add(ModItems.VIBRANIUM_NUGGET,
+                        ModItems.VULPUS_NUGGET,
+                        ModItems.ENDERIUM_NUGGET);
+
+        valueLookupBuilder(ConventionalItemTags.RAW_MATERIALS)
+                .add(ModItems.RAW_VIBRANIUM,
+                        ModItems.RAW_VULPUS,
+                        ModItems.RAW_ENDERIUM);
+
+        valueLookupBuilder(ItemTags.BREAKS_DECORATED_POTS).add(ModItems.VIBRANIUM_HAMMER,
+                ModItems.VULPUS_HAMMER,
+                ModItems.ENDERIUM_HAMMER,
+                ModItems.VIBRANIUM_EXCAVATOR,
+                ModItems.VULPUS_EXCAVATOR,
+                ModItems.ENDERIUM_EXCAVATOR,
+                ModItems.VIBRANIUM_PICKAXE,
+                ModItems.VULPUS_PICKAXE,
+                ModItems.ENDERIUM_PICKAXE,
+                ModItems.VIBRANIUM_HOE,
+                ModItems.VULPUS_HOE,
+                ModItems.ENDERIUM_HOE,
+                ModItems.VIBRANIUM_AXE,
+                ModItems.VULPUS_AXE,
+                ModItems.ENDERIUM_AXE,
+                ModItems.VIBRANIUM_SHOVEL,
+                ModItems.VULPUS_SHOVEL,
+                ModItems.ENDERIUM_SHOVEL,
+                ModItems.VIBRANIUM_PAXEL,
+                ModItems.VULPUS_PAXEL,
+                ModItems.ENDERIUM_PAXEL);
     }
 }
