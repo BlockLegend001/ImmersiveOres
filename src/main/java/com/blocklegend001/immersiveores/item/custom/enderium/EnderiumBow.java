cd76d74877b5ad5836514e003b4c1861769edc00
BlockLegend001 <rosarioarcamone@gmail.com>
2025-09-01 13:19:06 +0200

    Fixed Punch on bows causing arrows to drop at the player's feet when shooting upward

--- src/main/java/com/blocklegend001/immersiveores/item/custom/enderium/EnderiumBow.java
+++ src/main/java/com/blocklegend001/immersiveores/item/custom/enderium/EnderiumBow.java
@@ -92,9 +92,12 @@ public class EnderiumBow extends BowItem {
                                 .normalize()
                                 .multiply(punchLevel * 0.6 * resistance);
 
-                        arrowEntity.setVelocity(knockbackVec.x, 0.1, knockbackVec.z);
+                        arrowEntity.setVelocity(
+                                arrowEntity.getVelocity().x + knockbackVec.x,
+                                arrowEntity.getVelocity().y + knockbackVec.y,
+                                arrowEntity.getVelocity().z + knockbackVec.z
+                        );
                     }
-
                     PersistentProjectileEntity.PickupPermission pickupPermission = hasInfinity ? PersistentProjectileEntity.PickupPermission.DISALLOWED : PersistentProjectileEntity.PickupPermission.ALLOWED;
 
                     if (i == 0) {
