32ea774403e9ca5d5f5960d1b2388280061f2a42
BlockLegend001 <rosarioarcamone@gmail.com>
2025-10-17 17:04:37 +0200

    Check CHANGELOG.md

--- src/main/java/com/blocklegend001/immersiveores/blocks/ModBlocks.java
+++ src/main/java/com/blocklegend001/immersiveores/blocks/ModBlocks.java
@@ -26,47 +26,47 @@ public class ModBlocks {
 
     public static final Block VIBRANIUM_BLOCK = registerBlock("vibranium_block",
             new VibraniumBlock(AbstractBlock.Settings.copy(Blocks.NETHERITE_BLOCK)
-                    .strength(6.5f).requiresTool()
+                    .strength(6.5f, 1200.0F).requiresTool()
                     .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ImmersiveOres.MOD_ID, "vibranium_block")))));
 
     public static final Block VULPUS_BLOCK = registerBlock("vulpus_block",
             new VulpusBlock(AbstractBlock.Settings.copy(ModBlocks.VIBRANIUM_BLOCK)
-                    .strength(6.5f).requiresTool()
+                    .strength(6.5f, 1200.0F).requiresTool()
                     .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ImmersiveOres.MOD_ID, "vulpus_block")))));
 
     public static final Block ENDERIUM_BLOCK = registerBlock("enderium_block",
             new EnderiumBlock(AbstractBlock.Settings.copy(ModBlocks.VULPUS_BLOCK)
-                    .strength(6.5f).requiresTool()
+                    .strength(6.5f, 1200.0F).requiresTool()
                     .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ImmersiveOres.MOD_ID, "enderium_block")))));
 
     public static final Block RAW_VIBRANIUM_BLOCK = registerBlock("raw_vibranium_block",
             new RawVibraniumBlock(AbstractBlock.Settings.copy(Blocks.NETHERITE_BLOCK)
-                    .strength(6.5f).requiresTool()
+                    .strength(6.5f, 1200.0F).requiresTool()
                     .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ImmersiveOres.MOD_ID, "raw_vibranium_block")))));
 
     public static final Block RAW_VULPUS_BLOCK = registerBlock("raw_vulpus_block",
             new RawVulpusBlock(AbstractBlock.Settings.copy(ModBlocks.RAW_VIBRANIUM_BLOCK)
-                    .strength(6.5f).requiresTool()
+                    .strength(6.5f, 1200.0F).requiresTool()
                     .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ImmersiveOres.MOD_ID, "raw_vulpus_block")))));
 
     public static final Block RAW_ENDERIUM_BLOCK = registerBlock("raw_enderium_block",
             new RawEnderiumBlock(AbstractBlock.Settings.copy(ModBlocks.RAW_VULPUS_BLOCK)
-                    .strength(6.5f).requiresTool()
+                    .strength(6.5f, 1200.0F).requiresTool()
                     .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ImmersiveOres.MOD_ID, "raw_enderium_block")))));
 
     public static final Block VIBRANIUM_ORE = registerBlock("vibranium_ore",
             new VibraniumOre(AbstractBlock.Settings.copy(Blocks.ANCIENT_DEBRIS)
-                    .strength(6f).requiresTool()
+                    .strength(6.5f, 1200.0F).requiresTool()
                     .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ImmersiveOres.MOD_ID, "vibranium_ore")))));
 
     public static final Block VULPUS_ORE = registerBlock("vulpus_ore",
             new VulpusOre(AbstractBlock.Settings.copy(ModBlocks.VIBRANIUM_ORE)
-                    .strength(6f).requiresTool()
+                    .strength(6.5f, 1200.0F).requiresTool()
                     .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ImmersiveOres.MOD_ID, "vulpus_ore")))));
 
     public static final Block ENDERIUM_ORE = registerBlock("enderium_ore",
             new EnderiumOre(AbstractBlock.Settings.copy(ModBlocks.VULPUS_ORE)
-                    .strength(6f).requiresTool()
+                    .strength(6.5f, 1200.0F).requiresTool()
                     .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ImmersiveOres.MOD_ID, "enderium_ore")))));
 
 
