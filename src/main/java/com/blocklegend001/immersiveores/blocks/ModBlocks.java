a631ad32d89a25d8c499626c95ea600ed7eac32f
BlockLegend001 <rosarioarcamone@gmail.com>
2025-10-18 12:34:16 +0200

    Check CHANGELOG.md

--- src/main/java/com/blocklegend001/immersiveores/blocks/ModBlocks.java
+++ src/main/java/com/blocklegend001/immersiveores/blocks/ModBlocks.java
@@ -34,39 +34,39 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> VIBRANIUM_BLOCK = registerBlock("vibranium_block",
             () -> new VibraniumBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERITE_BLOCK)
-                    .strength(6.5f).requiresCorrectToolForDrops()));
+                    .strength(6.5f, 1200.0F).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> VULPUS_BLOCK = registerBlock("vulpus_block",
             () -> new VulpusBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.VIBRANIUM_BLOCK.get())
-                    .strength(6.5f).requiresCorrectToolForDrops()));
+                    .strength(6.5f, 1200.0F).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> ENDERIUM_BLOCK = registerBlock("enderium_block",
             () -> new EnderiumBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.VULPUS_BLOCK.get())
-                    .strength(6.5f).requiresCorrectToolForDrops()));
+                    .strength(6.5f, 1200.0F).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> RAW_VIBRANIUM_BLOCK = registerBlock("raw_vibranium_block",
             () -> new RawVibraniumBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERITE_BLOCK)
-                    .strength(6.5f).requiresCorrectToolForDrops()));
+                    .strength(6.5f, 1200.0F).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> RAW_VULPUS_BLOCK = registerBlock("raw_vulpus_block",
             () -> new RawVulpusBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.RAW_VIBRANIUM_BLOCK.get())
-                    .strength(6.5f).requiresCorrectToolForDrops()));
+                    .strength(6.5f, 1200.0F).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> RAW_ENDERIUM_BLOCK = registerBlock("raw_enderium_block",
             () -> new RawEnderiumBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.RAW_VULPUS_BLOCK.get())
-                    .strength(6.5f).requiresCorrectToolForDrops()));
+                    .strength(6.5f, 1200.0F).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> VIBRANIUM_ORE = registerBlock("vibranium_ore",
             () -> new VibraniumOre(BlockBehaviour.Properties.ofFullCopy(Blocks.ANCIENT_DEBRIS)
-                    .strength(6f).requiresCorrectToolForDrops()));
+                    .strength(6f, 1200.0F).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> VULPUS_ORE = registerBlock("vulpus_ore",
             () -> new VulpusOre(BlockBehaviour.Properties.ofFullCopy(ModBlocks.VIBRANIUM_ORE.get())
-                    .strength(6f).requiresCorrectToolForDrops()));
+                    .strength(6f, 1200.0F).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> ENDERIUM_ORE = registerBlock("enderium_ore",
             () -> new EnderiumOre(BlockBehaviour.Properties.ofFullCopy(ModBlocks.VULPUS_ORE.get())
-                    .strength(6f).requiresCorrectToolForDrops()));
+                    .strength(6f, 1200.0F).requiresCorrectToolForDrops()));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
@@ -86,8 +86,6 @@ public class ModBlocks {
             }
         });
     }
-
-
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
