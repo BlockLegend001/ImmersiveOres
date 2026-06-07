98c6a5e3b2f0bc0659fae16a55062a0845c65d8c
BlockLegend001 <rosarioarcamone@gmail.com>
2026-06-07 11:38:50 +0200

    Added 26.1.2-NeoForge

---
+++ src/main/java/com/blocklegend001/immersiveores/item/ModArmorMaterials.java
@@ -0,0 +1,49 @@
+package com.blocklegend001.immersiveores.item;
+
+import com.blocklegend001.immersiveores.config.EnderiumConfig;
+import com.blocklegend001.immersiveores.config.VibraniumConfig;
+import com.blocklegend001.immersiveores.config.VulpusConfig;
+import com.blocklegend001.immersiveores.util.ModEquipmentAssets;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.Util;
+import net.minecraft.world.item.equipment.ArmorMaterial;
+import net.minecraft.world.item.equipment.ArmorType;
+
+import java.util.EnumMap;
+
+public final class ModArmorMaterials {
+
+    public static final ArmorMaterial VIBRANIUM = new ArmorMaterial(VibraniumConfig.DURABILITY_VIBRANIUM.get(), Util.make(new EnumMap<>(ArmorType.class),
+            attribute -> {
+                attribute.put(ArmorType.BOOTS, VibraniumConfig.PROTECTION_VALUE_VIBRANIUM_BOOTS.get());
+                attribute.put(ArmorType.LEGGINGS, VibraniumConfig.PROTECTION_VALUE_VIBRANIUM_LEGGINGS.get());
+                attribute.put(ArmorType.CHESTPLATE, VibraniumConfig.PROTECTION_VALUE_VIBRANIUM_CHESTPLATE.get());
+                attribute.put(ArmorType.HELMET, VibraniumConfig.PROTECTION_VALUE_VIBRANIUM_HELMET.get());
+            }), VibraniumConfig.ENCHANTMENT_VALUE_VIBRANIUM_ARMOR.get(), SoundEvents.ARMOR_EQUIP_NETHERITE,
+            VibraniumConfig.TOUGHNESS_VALUE_VIBRANIUM_ARMOR.get(), (float) VibraniumConfig.KNOCKBACK_RESISTANCE_VALUE_VIBRANIUM_ARMOR.getAsDouble(),
+            ItemTags.REPAIRS_NETHERITE_ARMOR,
+            ModEquipmentAssets.VIBRANIUM);
+
+    public static final ArmorMaterial VULPUS = new ArmorMaterial(VulpusConfig.DURABILITY_VULPUS.get(), Util.make(new EnumMap<>(ArmorType.class),
+            attribute -> {
+                attribute.put(ArmorType.BOOTS, VulpusConfig.PROTECTION_VALUE_VULPUS_BOOTS.get());
+                attribute.put(ArmorType.LEGGINGS, VulpusConfig.PROTECTION_VALUE_VULPUS_LEGGINGS.get());
+                attribute.put(ArmorType.CHESTPLATE, VulpusConfig.PROTECTION_VALUE_VULPUS_CHESTPLATE.get());
+                attribute.put(ArmorType.HELMET, VulpusConfig.PROTECTION_VALUE_VULPUS_HELMET.get());
+            }), VulpusConfig.ENCHANTMENT_VALUE_VULPUS_ARMOR.get(), SoundEvents.ARMOR_EQUIP_NETHERITE,
+            VulpusConfig.TOUGHNESS_VALUE_VULPUS_ARMOR.get(), (float) VulpusConfig.KNOCKBACK_RESISTANCE_VALUE_VULPUS_ARMOR.getAsDouble(),
+            ItemTags.REPAIRS_NETHERITE_ARMOR,
+            ModEquipmentAssets.VULPUS);
+
+    public static final ArmorMaterial ENDERIUM = new ArmorMaterial(EnderiumConfig.DURABILITY_ENDERIUM.get(), Util.make(new EnumMap<>(ArmorType.class),
+            attribute -> {
+                attribute.put(ArmorType.BOOTS, EnderiumConfig.PROTECTION_VALUE_ENDERIUM_BOOTS.get());
+                attribute.put(ArmorType.LEGGINGS, EnderiumConfig.PROTECTION_VALUE_ENDERIUM_LEGGINGS.get());
+                attribute.put(ArmorType.CHESTPLATE, EnderiumConfig.PROTECTION_VALUE_ENDERIUM_CHESTPLATE.get());
+                attribute.put(ArmorType.HELMET, EnderiumConfig.PROTECTION_VALUE_ENDERIUM_HELMET.get());
+            }), EnderiumConfig.ENCHANTMENT_VALUE_ENDERIUM_ARMOR.get(), SoundEvents.ARMOR_EQUIP_NETHERITE,
+            EnderiumConfig.TOUGHNESS_VALUE_ENDERIUM_ARMOR.get(), (float) EnderiumConfig.KNOCKBACK_RESISTANCE_VALUE_ENDERIUM_ARMOR.getAsDouble(),
+            ItemTags.REPAIRS_NETHERITE_ARMOR,
+            ModEquipmentAssets.ENDERIUM);
+}
---
+++ src/main/java/com/blocklegend001/immersiveores/item/custom/vibranium/VibraniumPickaxe.java
@@ -0,0 +1,60 @@
+package com.blocklegend001.immersiveores.item.custom.vibranium;
+
+import com.blocklegend001.immersiveores.config.VibraniumConfig;
+import com.blocklegend001.immersiveores.item.ModToolTiers;
+import com.blocklegend001.immersiveores.util.ScreenUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.util.Unit;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ToolMaterial;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
+import net.minecraft.world.level.block.Block;
+
+import java.util.function.Consumer;
+
+public class VibraniumPickaxe extends Item {
+    private static Properties createSettings(Properties properties, boolean unbreakable, int durability) {
+        properties.durability(durability);
+
+        if (unbreakable) {
+            properties.component(DataComponents.UNBREAKABLE, Unit.INSTANCE);
+        }
+        return properties;
+    }
+
+    public VibraniumPickaxe(ModToolTiers material, float attackDamage, float attackSpeed, Properties settings) {
+        super(
+                material.applyToolProperties(
+                        createSettings(settings, VibraniumConfig.UNBREAKABLE_VIBRANIUM.get(), VibraniumConfig.DURABILITY_VIBRANIUM.get()),
+                        BlockTags.MINEABLE_WITH_PICKAXE,
+                        attackDamage,
+                        attackSpeed
+                )
+        );
+    }
+
+    @Override
+    public Component getName(ItemStack stack) {
+        return super.getName(stack).copy().withStyle(ChatFormatting.LIGHT_PURPLE);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, TooltipContext p_333372_, TooltipDisplay p_396484_, Consumer<Component> consumer, TooltipFlag p_41424_) {
+        super.appendHoverText(pStack, p_333372_, p_396484_, consumer, p_41424_);
+        if(ScreenUtils.isShiftDown()) {
+            if (VibraniumConfig.UNBREAKABLE_VIBRANIUM.get()) {
+                consumer.accept(Component.translatable("tooltip.immersiveores.unbreakble.tooltip").withStyle(ChatFormatting.LIGHT_PURPLE));
+            }
+            consumer.accept(Component.translatable("tooltip.immersiveores.immunetofire.tooltip").withStyle(ChatFormatting.LIGHT_PURPLE));
+        } else {
+            consumer.accept(Component.translatable("tooltip.immersiveores.pressshiftformoreinfo.tooltip").withStyle(ChatFormatting.LIGHT_PURPLE));
+        }
+    }
+}
