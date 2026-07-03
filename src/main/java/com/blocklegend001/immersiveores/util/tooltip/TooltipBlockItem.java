fc751dbab4507c9792e029b0a4e0af049a2d7e02
BlockLegend001 <rosarioarcamone@gmail.com>
2026-07-03 22:13:43 +0200

    Fixed Block Tooltip not showed correctly

--- src/main/java/com/blocklegend001/immersiveores/util/tooltip/TooltipBlockItem.java
+++ src/main/java/com/blocklegend001/immersiveores/util/tooltip/TooltipBlockItem.java
@@ -1,15 +1,17 @@
 package com.blocklegend001.immersiveores.util.tooltip;
 
-import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.Nullable;
 
+/**
+ * Represents a block item that has a tooltip.<br>
+ * Basically redirects the tooltip to the corresponding block extending {@link TooltipItem} in the registry.
+ */
 public class TooltipBlockItem extends BlockItem implements TooltipItem {
     public TooltipBlockItem(TooltipBlock block, Properties settings) {
         super((Block) block, settings);
@@ -18,11 +20,7 @@ public class TooltipBlockItem extends BlockItem implements TooltipItem {
     @Override
     public void appendClientTooltip(ItemStack stack, TooltipAccept tooltips) {
         Identifier value = stack.getItem().components().get(DataComponents.ITEM_MODEL);
-        if (value == null) return;
-
-        ResourceKey<Block> key = ResourceKey.create(Registries.BLOCK, value);
-        Holder.Reference<Block> block = BuiltInRegistries.BLOCK.get(key).orElse(null);
-
+        @Nullable Block block = BuiltInRegistries.BLOCK.getValue(value);
         if (block instanceof TooltipItem tooltipItem) {
             tooltipItem.appendClientTooltip(stack, tooltips);
         }
