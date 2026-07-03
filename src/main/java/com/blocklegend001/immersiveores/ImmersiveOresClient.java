e2a4f8c62ad03d61f531c474d886c7b8581898dc
BlockLegend001 <rosarioarcamone@gmail.com>
2026-07-03 21:52:55 +0200

    Added KeyBind to Enable/Disable the Outline

--- src/main/java/com/blocklegend001/immersiveores/ImmersiveOresClient.java
+++ src/main/java/com/blocklegend001/immersiveores/ImmersiveOresClient.java
@@ -6,14 +6,49 @@ import com.blocklegend001.immersiveores.util.tools.hammer.*;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class ImmersiveOresClient implements ClientModInitializer {
     private static boolean shiftKeyPressed = false;
 
+    public static KeyBinding SHOW_OUTLINE_KEY;
+    public static boolean SHOW_OUTLINE_ENABLED = true;
+
+    private static final KeyBinding.Category CATEGORY =
+            KeyBinding.Category.create(
+                    Identifier.of("immersiveores", "showoutline")
+            );
+
     @Override
     public void onInitializeClient() {
+        SHOW_OUTLINE_KEY = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.immersiveores.showoutline",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_O,
+                        CATEGORY
+                )
+        );
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (SHOW_OUTLINE_KEY.wasPressed()) {
+                SHOW_OUTLINE_ENABLED = !SHOW_OUTLINE_ENABLED;
+
+                if (client.player != null) {
+                    client.player.sendMessage(
+                            Text.literal("Outline: " + (SHOW_OUTLINE_ENABLED ? "ON" : "OFF")),
+                            false
+                    );
+                }
+            }
+        });
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> updateShiftKey());
         ItemTooltipCallback.EVENT.register((itemStack, tooltipContext, tooltipType, list
         ) -> {
