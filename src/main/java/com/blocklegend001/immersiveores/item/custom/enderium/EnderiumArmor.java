42eb74a862fbc79c3959f6be6b52c935e64cd51e
BlockLegend001 <rosarioarcamone@gmail.com>
2026-07-14 23:02:20 +0200

    Now when you craft the Armors the Trim will be transferred

--- src/main/java/com/blocklegend001/immersiveores/item/custom/enderium/EnderiumArmor.java
+++ src/main/java/com/blocklegend001/immersiveores/item/custom/enderium/EnderiumArmor.java
@@ -21,6 +21,7 @@ import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.item.equipment.ArmorMaterial;
 import net.minecraft.world.item.equipment.ArmorType;
 import net.minecraft.world.item.equipment.Equippable;
+import net.minecraft.world.item.equipment.trim.ArmorTrim;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
@@ -118,6 +119,11 @@ public class EnderiumArmor extends Item {
                 var customName = ingredient.get(DataComponents.CUSTOM_NAME);
                 if (customName != null) {
                     stack.set(DataComponents.CUSTOM_NAME, customName);
+                }
+
+                ArmorTrim trim = ingredient.get(DataComponents.TRIM);
+                if (trim != null) {
+                    stack.set(DataComponents.TRIM, trim);
                 }
 
                 break;
