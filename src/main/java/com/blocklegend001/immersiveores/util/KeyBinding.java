28357bfa6ffe75f78ff5fb3ac3d2f98b2535f2a9
BlockLegend001 <rosarioarcamone@gmail.com>
2026-07-17 15:33:40 +0200

    Added KeyBind to Enable/Disable the Outline

---
+++ src/main/java/com/blocklegend001/immersiveores/util/KeyBinding.java
@@ -0,0 +1,29 @@
+package com.blocklegend001.immersiveores.util;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import org.lwjgl.glfw.GLFW;
+
+import static com.blocklegend001.immersiveores.ImmersiveOres.MODID;
+
+public class KeyBinding {
+    public static final KeyMapping.Category IMMERSIVEORES_CATEGORY =
+            KeyMapping.Category.register(
+                    Identifier.fromNamespaceAndPath(MODID, "showoutline")
+            );
+
+    public static final KeyMapping SHOW_OUTLINE_KEY = new KeyMapping(
+            "key.immersiveores.showoutline",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_O,
+            IMMERSIVEORES_CATEGORY
+    );
+
+    @SubscribeEvent
+    public static void registerKeys(RegisterKeyMappingsEvent event) {
+        event.register(SHOW_OUTLINE_KEY);
+    }
+}
