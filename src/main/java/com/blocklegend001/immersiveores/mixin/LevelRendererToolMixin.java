1708a66a65741a88a749c10b817ad7b5d09faa55
BlockLegend001 <rosarioarcamone@gmail.com>
2026-07-17 15:16:38 +0200

    Added KeyBind to Enable/Disable the Outline

--- src/main/java/com/blocklegend001/immersiveores/mixin/LevelRendererToolMixin.java
+++ src/main/java/com/blocklegend001/immersiveores/mixin/LevelRendererToolMixin.java
@@ -1,5 +1,6 @@
 package com.blocklegend001.immersiveores.mixin;
 
+import com.blocklegend001.immersiveores.ImmersiveOres;
 import com.blocklegend001.immersiveores.item.custom.base.Excavator;
 import com.blocklegend001.immersiveores.item.custom.base.Hammer;
 import com.blocklegend001.immersiveores.util.map.RadiusMap;
@@ -33,6 +34,7 @@ public class LevelRendererToolMixin {
     @Inject(method = "renderLevel", at = @At("TAIL"))
     private void renderLevelAfter(GraphicsResourceAllocator allocator, DeltaTracker delta, boolean tick, Camera camera, Matrix4f matrix4f1, Matrix4f matrix4f2, GpuBufferSlice gpu, Vector4f vec4f, boolean b, CallbackInfo ci) {
         Minecraft mc = Minecraft.getInstance();
+        if (!ImmersiveOres.SHOW_OUTLINE_ENABLED) return;
         if (mc.level == null || mc.player == null) return;
 
         ItemStack heldItem = mc.player.getMainHandItem();
