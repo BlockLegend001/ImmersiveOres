cbc488ad10da7d918c48818e16b9e9576ca79594
BlockLegend001 <rosarioarcamone@gmail.com>
2026-07-17 15:06:59 +0200

    Added KeyBind to Enable/Disable the Outline

--- src/main/java/com/blocklegend001/immersiveores/ImmersiveOres.java
+++ src/main/java/com/blocklegend001/immersiveores/ImmersiveOres.java
@@ -19,6 +19,7 @@ import net.minecraftforge.fml.loading.FMLPaths;
 @Mod(ImmersiveOres.MODID)
 public class ImmersiveOres {
     public static final String MODID = "immersiveores";
+    public static boolean SHOW_OUTLINE_ENABLED = true;
 
     public ImmersiveOres(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
