acc4c4e902224328261b02ee5fd9dcca0efba83c
BlockLegend001 <rosarioarcamone@gmail.com>
2026-08-07 16:09:09 +0200

    Fixed a rare crash on startup caused by a bug on bow registration

--- src/main/java/com/blocklegend001/immersiveores/ImmersiveOres.java
+++ src/main/java/com/blocklegend001/immersiveores/ImmersiveOres.java
@@ -45,10 +45,9 @@ public class ImmersiveOres {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-
     @SubscribeEvent
     public void clientSetup(final FMLClientSetupEvent event) {
-        ModItemProperties.addCustomItemProperties();
+        event.enqueueWork(ModItemProperties::addCustomItemProperties);
     }
 
     @SubscribeEvent
