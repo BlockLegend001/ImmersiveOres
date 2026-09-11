2b90561ea853ddaac7cc19df213ff35faff5b97b
BlockLegend001 <rosarioarcamone@gmail.com>
2026-09-11 10:41:34 +0200

    Small Bug Fix

--- src/main/java/com/blocklegend001/immersiveores/util/tools/excavator/ModEventsVibraniumExcavator.java
+++ src/main/java/com/blocklegend001/immersiveores/util/tools/excavator/ModEventsVibraniumExcavator.java
@@ -25,6 +25,8 @@ public class ModEventsVibraniumExcavator implements PlayerBlockBreakEvents.Befor
 
         ItemStack mainHandItem = player.getMainHandStack();
         if (!(mainHandItem.getItem() instanceof VibraniumExcavator excavator)) return true;
+        if (state.getHardness(world, pos) == 0.0F)  return true;
+
         if (HARVESTED_BLOCKS.contains(pos)) return true;
 
         boolean isSneaking = player.isSneaking();
